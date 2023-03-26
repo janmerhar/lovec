efabbade745e1ae4964b91e2abb8103c78371833
Jan Merhar <merharjan@gmail.com>
2023-03-26 16:01:43 +0200

    Improve app layout in views/TabsPage.vue

--- app/src/views/TabsPage.vue
+++ app/src/views/TabsPage.vue
@@ -1,28 +1,52 @@
 <template>
   <ion-page>
     <ion-tabs>
+      <!-- Top tool bar -->
+      <app-toolbar></app-toolbar>
+      <!-- Component view -->
       <ion-router-outlet></ion-router-outlet>
+      <!-- Bootom tool bar -->
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-label>Jage</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-label>Revirji</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-label>Beležke</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-label>Opazovalnica</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab5" href="/tabs/tab5">
+          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-label>Oprema</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
+      <!-- End bottom tool bar -->
     </ion-tabs>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonRouterOutlet,
+} from "@ionic/vue"
+import { ellipse, square, triangle } from "ionicons/icons"
+import AppToolbar from "@/components/AppToolbar.vue"
 </script>
