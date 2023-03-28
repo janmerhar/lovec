7518013cff0e9463eca29e74dce670d0ed91ff67
Jan Merhar <merharjan@gmail.com>
2023-03-28 18:52:02 +0200

    Add views/TabIzkaznica.vue

---
+++ app/src/views/TabIzkaznica.vue
@@ -0,0 +1,21 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Izkaznica</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue"
+</script>
