b39d5bd23acc6cd9b4fd7775cd87fe69b603de62
Jan Merhar <merharjan@gmail.com>
2023-03-30 19:30:50 +0200

    Add FontAwesome icons in views/TabsPage.vue

--- app/src/views/TabsPage.vue
+++ app/src/views/TabsPage.vue
@@ -8,28 +8,28 @@
       <!-- Bootom tool bar -->
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="jage" href="/tabs/jage">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <font-awesome-icon icon="fa-solid fa-people-group" size="2x" />
           <ion-label>Jage</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="revirji" href="/tabs/revirji">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Revirji</ion-label>
-        </ion-tab-button>
-
         <ion-tab-button tab="belezke" href="/tabs/belezke">
-          <ion-icon aria-hidden="true" :icon="square" />
+          <font-awesome-icon icon="fa-solid fa-note-sticky" size="2x" />
           <ion-label>Beležke</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="opazovalnica" href="/tabs/opazovalnica">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Opazovalnica</ion-label>
+          <font-awesome-icon icon="fa-solid fa-tower-observation" size="2x" />
+          <ion-label>Opazovalnice</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="oprema" href="/tabs/oprema">
-          <ion-icon aria-hidden="true" :icon="square" />
+          <font-awesome-icon icon="fa-solid fa-person-rifle" size="2x" />
           <ion-label>Oprema</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="revirji" href="/tabs/revirji">
+          <font-awesome-icon icon="fa-solid fa-map-location-dot" size="2x" />
+          <ion-label>Revirji</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
       <!-- End bottom tool bar -->
@@ -43,10 +43,8 @@ import {
   IonTabButton,
   IonTabs,
   IonLabel,
-  IonIcon,
   IonPage,
   IonRouterOutlet,
 } from "@ionic/vue"
-import { ellipse, square, triangle } from "ionicons/icons"
 import AppToolbar from "@/components/AppToolbar.vue"
 </script>
