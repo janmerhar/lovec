d76fd9c9d2183f8a6fe671d123696db668c8115b
Jan Merhar <merharjan@gmail.com>
2023-04-05 17:57:09 +0200

    Add component components/ModajJagaAdd.vue

---
+++ app/src/components/ModalJagaAdd.vue
@@ -0,0 +1,63 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-buttons slot="start">
+        <ion-button color="medium" @click="cancel">Prekliči</ion-button>
+      </ion-buttons>
+      <ion-title class="ion-text-center">Dodaj jago</ion-title>
+      <ion-buttons slot="end">
+        <ion-button @click="confirm">Potrdi</ion-button>
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content class="ion-padding">
+    <ion-item>
+      <ion-label position="stacked">Your name</ion-label>
+      <ion-input v-model="name" placeholder="Your name"></ion-input>
+    </ion-item>
+  </ion-content>
+</template>
+
+<script>
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonItem,
+  IonLabel,
+  IonInput,
+  modalController,
+} from "@ionic/vue"
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "ModalJagaAdd",
+  components: {
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonItem,
+    IonLabel,
+    IonInput,
+  },
+  data() {
+    return {
+      name: "",
+    }
+  },
+  methods: {
+    cancel() {
+      return modalController.dismiss(null, "cancel")
+    },
+    confirm() {
+      return modalController.dismiss(this.name, "confirm")
+    },
+  },
+})
+</script>
