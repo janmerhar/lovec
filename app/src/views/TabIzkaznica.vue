6411b01d6d28ac45a523abf7d471782b19fb9c94
Jan Merhar <merharjan@gmail.com>
2023-04-07 20:05:55 +0200

    Implement qrcode.vue in views/TabIzkaznica.vue

--- app/src/views/TabIzkaznica.vue
+++ app/src/views/TabIzkaznica.vue
@@ -1,16 +1,31 @@
 <template>
   <ion-page>
-    <ion-content :fullscreen="true">
+    <ion-content :fullscreen="true" class="ion-padding">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Izkaznica</ion-title>
         </ion-toolbar>
       </ion-header>
+
+      <div>
+        <ion-text color="primary">
+          <h5>Ime</h5>
+        </ion-text>
+        <ion-text color="dark"> Janez Novak </ion-text>
+      </div>
+
+      <br />
+      <qrcode-vue
+        :value="qr_value"
+        :size="1000"
+        level="H"
+        style="width: 100%; height: auto"
+      />
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script>
 import {
   IonPage,
   IonHeader,
@@ -18,4 +33,22 @@ import {
   IonTitle,
   IonContent,
 } from "@ionic/vue"
+import QrcodeVue from "qrcode.vue"
+import { defineComponent } from "vue"
+
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    QrcodeVue,
+  },
+  data() {
+    return {
+      qr_value: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    }
+  },
+}
 </script>
