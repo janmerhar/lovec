74c360f1ba7d551b393351fa2e31897ee3a452c7
Jan Merhar <merharjan@gmail.com>
2023-04-10 22:42:28 +0200

    Add views/belezke/SubTabVplen.vue

---
+++ app/src/views/belezke/SubTabVplen.vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Podrobnosti o vseh vplenih</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ExploreContainer name="Podrobnosti o vseh vplenih" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue"
+import ExploreContainer from "@/components/ExploreContainer.vue"
+</script>
