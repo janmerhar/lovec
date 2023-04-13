6c890b8d973da9562fad1e2e7b5d2a81c372e5da
Jan Merhar <merharjan@gmail.com>
2023-04-13 15:39:20 +0200

    Add components/belezke/CardVplen.vue

---
+++ app/src/components/belezke/CardVplen.vue
@@ -0,0 +1,49 @@
+<template>
+  <ion-card :button="true">
+    <ion-card-header>
+      <ion-card-subtitle>{{ lovec }}</ion-card-subtitle>
+      <ion-card-title>{{ datum }}</ion-card-title>
+    </ion-card-header>
+
+    <ion-card-content>
+      <!-- {{ vplen }} -->
+      Podrobnosti vplena, Podrobnosti vplena, Podrobnosti vplena, Podrobnosti
+      vplena, Podrobnosti vplena, Podrobnosti vplena, Podrobnosti vplena,
+      Podrobnosti vplena, Podrobnosti vplena, (Omejena dolžina, se preuredi)
+
+      <!-- 
+        Nacrt:
+        - naredi trim na cca 200 znakov ter jim pripni tri pike
+        - za vnos vzemi kar vnose iz tabele vplenov v vrsti objekta
+        - vnose nato preuredi v string
+        - Naj bo samo prvi znak v besedilu uppercase, vsi ostali na bodo lowercase
+       -->
+    </ion-card-content>
+    <ion-button fill="clear" @click.prevent="$emit('view')">Več</ion-button>
+  </ion-card>
+</template>
+
+<script lang="ts">
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonButton,
+} from "@ionic/vue"
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  components: {
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+    IonButton,
+  },
+  props: ["datum", "lovec", "vplen"],
+  emits: ["view"],
+})
+</script>
