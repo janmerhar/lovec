e9c66090666e19e8d5817234fb7d5c51b4e728c2
Jan Merhar <merharjan@gmail.com>
2023-04-14 17:52:16 +0200

    Add components/vplen/CardVplenDescription.vue

---
+++ app/src/components/vplen/CardVplenDescription.vue
@@ -0,0 +1,82 @@
+<template>
+  <ion-card class="ion-no-margin ion-no-padding ion-margin-bottom">
+    <ion-card-header
+      class="ion-no-padding ion-padding-horizontal ion-padding-top"
+    >
+      <ion-card-title>Ime vplena</ion-card-title>
+      <ion-card-subtitle>Teža 86 kg</ion-card-subtitle>
+    </ion-card-header>
+    <!--  -->
+    <ion-card-content>
+      <ion-list>
+        <!--  -->
+        <ion-item>
+          <ion-thumbnail slot="start">
+            <font-awesome-icon icon="fa-solid fa-square-virus" size="3x" />
+          </ion-thumbnail>
+          <ion-label>Bolezen</ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-thumbnail slot="start">
+            <font-awesome-icon icon="fa-solid fa-square-virus" size="3x" />
+          </ion-thumbnail>
+          <ion-label>Bolezen</ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-thumbnail slot="start">
+            <font-awesome-icon icon="fa-solid fa-square-virus" size="3x" />
+          </ion-thumbnail>
+          <ion-label>Bolezen</ion-label>
+        </ion-item>
+        <!--  -->
+      </ion-list>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script lang="ts">
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonThumbnail,
+  IonItem,
+  IonList,
+  IonLabel,
+} from "@ionic/vue"
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  components: {
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonThumbnail,
+    IonItem,
+    IonList,
+    IonLabel,
+  },
+})
+</script>
+
+<style scoped>
+ion-item {
+  --padding-start: 0;
+}
+
+ion-thumbnail {
+  height: 40px;
+  width: 40px;
+}
+
+font-awesome-icon {
+  height: 40px !important;
+  width: 40px !important;
+}
+</style>
