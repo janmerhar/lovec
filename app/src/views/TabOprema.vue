b21a6d1e21197364cf7ece9788b4684e72ef66ad
Jan Merhar <merharjan@gmail.com>
2023-04-15 13:58:00 +0200

    Implement components/oprema/CardOpremaDescription.vue and components/oprema/ModalOpremaAdd.vue in views/TabOprema.vue

--- app/src/views/TabOprema.vue
+++ app/src/views/TabOprema.vue
@@ -1,22 +1,81 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      <p>123</p>
-      <p>456</p>
-      <p>789</p>
-      <p>123</p>
-      <p>456</p>
-      <p>789</p>
+      <!--  -->
+      <h3 class="ion-text-center">Evidenca opreme za dan</h3>
+      <div class="ion-padding">
+        <ion-item fill="outline" class="">
+          <ion-label position="stacked">Datum</ion-label>
+          <ion-input
+            placeholder="Datum"
+            type="date"
+            :clear-input="true"
+            required
+          ></ion-input>
+        </ion-item>
+      </div>
+      <!--  -->
+      <card-oprema-description
+        :oprema="'Puška'"
+        datum="Ponedeljek, 13. 3. 2023"
+      ></card-oprema-description>
+      <card-oprema-description
+        :oprema="'Puška'"
+        datum="Ponedeljek, 13. 3. 2023"
+      ></card-oprema-description>
+      <card-oprema-description
+        :oprema="'Puška'"
+        datum="Ponedeljek, 13. 3. 2023"
+      ></card-oprema-description>
+      <!--  -->
+      <fab-button-add @click.prevent="openModalOpremaAdd"></fab-button-add>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import FabButtonAdd from "@/components/FabButtonAdd.vue"
+import ModalOpremaAdd from "@/components/oprema/ModalOpremaAdd.vue"
+import CardOpremaDescription from "@/components/oprema/CardOpremaDescription.vue"
+
 import {
   IonPage,
   // IonHeader,
   // IonToolbar,
   // IonTitle,
   IonContent,
+  modalController,
+  IonInput,
+  IonLabel,
+  IonItem,
 } from "@ionic/vue"
+
+export default {
+  components: {
+    IonPage,
+    // IonHeader,
+    // IonToolbar,
+    // IonTitle,
+    IonContent,
+    FabButtonAdd,
+    CardOpremaDescription,
+    IonInput,
+    IonLabel,
+    IonItem,
+  },
+  methods: {
+    async openModalOpremaAdd() {
+      const modal = await modalController.create({
+        component: ModalOpremaAdd,
+      })
+      modal.present()
+
+      // const { data, role } = await modal.onWillDismiss()
+
+      // if (role === "confirm") {
+      //   this.message = `Hello, ${data}!`
+      // }
+    },
+  },
+}
 </script>
