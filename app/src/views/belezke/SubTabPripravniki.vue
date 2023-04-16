a5e3e8704023ad737e77d3385de03bfe5af1ef1e
Jan Merhar <merharjan@gmail.com>
2023-04-16 20:25:21 +0200

    Implement pripravniki/ModalDnevnikAdd.vue and pripravniki/CardDnevnikDescription.vue in views/belezke/SubTabPripravniki.vue

--- app/src/views/belezke/SubTabPripravniki.vue
+++ app/src/views/belezke/SubTabPripravniki.vue
@@ -1,24 +1,72 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Podrobnosti o vseh pripravnikih</ion-title>
-        </ion-toolbar>
-      </ion-header>
+      <!--  -->
+      <h3 class="ion-text-center">Dnevniki za dan</h3>
+      <div class="ion-padding">
+        <ion-item fill="outline" class="">
+          <ion-label position="stacked">Datum</ion-label>
+          <ion-input
+            placeholder="Datum"
+            type="date"
+            :clear-input="true"
+            required
+          ></ion-input>
+        </ion-item>
+      </div>
+      <!--  -->
+      <card-dnevnik-description
+        :oprema="'Puška'"
+        datum="Ponedeljek, 13. 3. 2023"
+      ></card-dnevnik-description>
+      <card-dnevnik-description
+        :oprema="'Puška'"
+        datum="Ponedeljek, 13. 3. 2023"
+      ></card-dnevnik-description>
+      <card-dnevnik-description
+        :oprema="'Puška'"
+        datum="Ponedeljek, 13. 3. 2023"
+      ></card-dnevnik-description>
 
-      <ExploreContainer name="Podrobnosti o vseh pripravnikih" />
+      <!--  -->
+
+      <!-- Gumb samo za pripravnika, da vnese porocilo -->
+      <fab-button-add @click.prevent="openModalPripravnikAdd"></fab-button-add>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import {
+  modalController,
   IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
   IonContent,
+  IonLabel,
+  IonItem,
+  IonInput,
 } from "@ionic/vue"
-import ExploreContainer from "@/components/ExploreContainer.vue"
+
+import FabButtonAdd from "@/components/FabButtonAdd.vue"
+import ModalDnevnikAdd from "@/components/pripravniki/ModalDnevnikAdd.vue"
+import CardDnevnikDescription from "@/components/pripravniki/CardDnevnikDescription.vue"
+
+export default {
+  components: {
+    IonPage,
+    IonContent,
+    FabButtonAdd,
+    CardDnevnikDescription: CardDnevnikDescription,
+    IonLabel,
+    IonItem,
+    IonInput,
+  },
+  methods: {
+    async openModalPripravnikAdd() {
+      const modal = await modalController.create({
+        component: ModalDnevnikAdd,
+      })
+      modal.present()
+    },
+  },
+}
 </script>
