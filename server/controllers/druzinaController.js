38bceb17c8cdbaa395865a5bb4d0b45903c44aae
Jan Merhar <merharjan@gmail.com>
2023-04-26 18:33:00 +0200

    Add template in controllers/druzinaController.js

---
+++ server/controllers/druzinaController.js
@@ -0,0 +1,15 @@
+exports.getDruzina = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
+
+// TI si mogoce redundanten
+exports.getRevirji = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
+
+exports.getClani = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
