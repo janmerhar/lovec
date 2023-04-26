1e3c8e08ef41a600393b99e86d5bb1d3f61ff451
Jan Merhar <merharjan@gmail.com>
2023-04-26 18:31:41 +0200

    Add template in controllers/authController.js

---
+++ server/controllers/authController.js
@@ -0,0 +1,9 @@
+exports.postLogin = (req, res, next) => {
+  console.log("postLogin")
+  res.send("postLogin")
+}
+
+exports.postRegister = (req, res, next) => {
+  console.log("postRegister")
+  res.send("postRegister")
+}
