5d570cbabcb1c2b952ab443e06d71115a5dd198f
Jan Merhar <merharjan@gmail.com>
2023-04-27 19:40:51 +0200

    Add template in controllers/opremaController.js

---
+++ server/controllers/opremaController.js
@@ -0,0 +1,14 @@
+exports.postOpremaAdd = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
+
+exports.getOpremaDate = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
+
+exports.getOpremaRange = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
