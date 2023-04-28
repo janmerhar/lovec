400671fe0f9f29d9a6aea581368dea16fa2f0548
Jan Merhar <merharjan@gmail.com>
2023-04-28 20:41:17 +0200

    Add template in controllers/pripravnikController.js

---
+++ server/controllers/pripravnikController.js
@@ -0,0 +1,30 @@
+//
+// Mentor
+//
+
+// Dnevnik za dnevnike prirpavnikov za nekega mentorja za neki dan
+exports.getDnevnikPripravniki = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
+
+exports.patchSpremeniStatus = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
+
+//
+// Pripravnik
+//
+
+// Vpogled v svoj dnenvik za pripravnika
+// ta nima svojega dneva, temvec continuos scroll
+exports.getDnevnikPripravnik = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
+
+exports.postDnevnikVnesi = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
