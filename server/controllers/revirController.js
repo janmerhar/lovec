e23d6561b49ec482c595b55c244fc9d385bc0f78
Jan Merhar <merharjan@gmail.com>
2023-04-28 20:44:38 +0200

    Add template in controllers/revirController.js

---
+++ server/controllers/revirController.js
@@ -0,0 +1,10 @@
+exports.getRevirji = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
+
+// Tebe mogoce ne bom potreboval
+exports.getRevir = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
