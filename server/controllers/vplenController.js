4b200601c2c33705d1cde49d8aa66485cdcfcc00
Jan Merhar <merharjan@gmail.com>
2023-04-30 23:19:01 +0200

    Add controllers/vplenController.js

---
+++ server/controllers/vplenController.js
@@ -0,0 +1,17 @@
+// Tukaj imam infinite scroll, ki mi vraca po zadnjih n vplenov
+exports.getVpleni = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
+
+// Tukaj dobim samo podatke o enem vplenu
+// ce jih se nisem prej vseh pridobil
+exports.getVplen = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
+
+exports.postVplen = (req, res, next) => {
+  console.log("getUser")
+  res.send("getUser")
+}
