c981c7137ed946068353016727bb9d09c3714627
Jan Merhar <merharjan@gmail.com>
2023-05-03 18:52:37 +0200

    Add routes from routes/ in server/server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,13 @@ app.use(express.static(__dirname + "/views/"))
 
 // Defining route middleware
 app.use("/api", require("./routes/api"))
-app.use(require("./routes/auth"))
+app.use("/", require("./routes/auth"))
+app.use("/druzina", require("./routes/druzina"))
+app.use("/opazovalnice", require("./routes/opazovalnica"))
+app.use("/oprema", require("./routes/oprema"))
+app.use("/pripravniki", require("./routes/pripravnik"))
+app.use("/revirji", require("./routes/revir"))
+app.use("/uporabnik", require("./routes/uporabnik"))
 
 // Listening to port
 app.listen(port)
