a75bb7b172c2db43b559bb0c1d22d1748698f688
Jan Merhar <merharjan@gmail.com>
2023-07-14 22:27:43 +0200

    Add util/ErrorHandler.js

---
+++ server/util/ErrorHandler.js
@@ -0,0 +1,13 @@
+module.exports.ErrorHandler = (err, req, res, next) => {
+  console.log("Middleware Error Hadnling")
+
+  const errStatus = err.statusCode || 500
+  const errMsg = err.message || "Prišlo je do napake."
+
+  res.status(errStatus).json({
+    success: false,
+    status: errStatus,
+    message: errMsg,
+    stack: process.env.NODE_ENV === "development" ? err.stack : {},
+  })
+}
