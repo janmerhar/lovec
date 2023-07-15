e4fa8ff5f31ebf79d7b63ece808fdec75299aeeb
Jan Merhar <merharjan@gmail.com>
2023-07-15 22:55:30 +0200

    Rename routes/pripravnik.js to routes/dnevnik.js

--- server/routes/dnevnik.js
+++ server/routes/dnevnik.js
@@ -5,9 +5,9 @@ const router = express.Router()
 
 const dnevnikController = require("../controllers/dnevnikController")
 
-router.get("/dnevniki/:datum", dnevnikController.getDnevnikPripravniki)
-router.patch("/dnevniki/status", dnevnikController.patchSpremeniStatus)
-router.get("/dnevnik/:zacetek/:page", dnevnikController.getDnevnikPripravnik)
+router.get("/:datum", dnevnikController.getDnevnikPripravniki)
+router.patch("/status", dnevnikController.patchSpremeniStatus)
+router.get("/:zacetek/:page", dnevnikController.getDnevnikPripravnik)
 router.post("/dodaj", dnevnikController.postDnevnikVnesi)
 
 module.exports = router
