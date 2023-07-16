2521abf4873d081dd93044165c1cd1ba35521c59
Jan Merhar <merharjan@gmail.com>
2023-07-17 00:35:28 +0200

    Add postOprema() in controllers/opremaController.js

--- server/controllers/opremaController.js
+++ server/controllers/opremaController.js
@@ -1,8 +1,18 @@
-const Oprema = require("../models/oprema")
+const Oprema = require("../entities/Oprema")
+const Uporabnik = require("../entities/UserRoles/Uporabnik")
+const UporabnikFactory = require("../entities/UserRoles/UporabnikFactory")
 
 exports.postOprema = async (req, res, next) => {
-  console.log("getUser")
-  res.send("getUser")
+  try {
+    const { uporabnikId } = await UporabnikFactory.JWTpayload(req)
+    const { naziv, tip, stanje } = req.body
+
+    const result = await Oprema.vnesiOprema(uporabnikId, naziv, tip, stanje)
+
+    res.send(result)
+  } catch (error) {
+    next(error)
+  }
 }
 
 exports.deleteOprema = async (req, res, next) => {
