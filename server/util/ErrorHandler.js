ef17e76ab7f07218406e99ce7f051de0c9ee6eb2
Jan Merhar <merharjan@gmail.com>
2023-07-16 02:32:32 +0200

    Improve readout in util/ErrorHandler.js

--- server/util/ErrorHandler.js
+++ server/util/ErrorHandler.js
@@ -1,8 +1,10 @@
 module.exports = (err, req, res, next) => {
-  console.log("Middleware Error Hadnling")
+  console.log("Middleware Error Handling")
 
   const errStatus = err.statusCode || 500
   const errMsg = err.message || "Prišlo je do napake."
+
+  console.log(err.stack)
 
   res.status(errStatus).json({
     success: false,
