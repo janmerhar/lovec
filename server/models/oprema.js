847932a6e6c15aa04c8e5e82acafcc41b11ff1d4
Jan Merhar <merharjan@gmail.com>
2023-07-16 23:52:29 +0200

    Update oprema model in models/oprema.js

--- server/models/oprema.js
+++ server/models/oprema.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose
 
 const opremaSchema = new Schema({
   lastnik: { type: Schema.Types.ObjectId, ref: "Uporabnik", required: false },
-  naziv: [{ type: String, required: true }],
+  naziv: { type: String, required: true },
   tip: { type: String, required: true },
   stanje: { type: String, required: true },
   // set the field `datum` to current date (time)
