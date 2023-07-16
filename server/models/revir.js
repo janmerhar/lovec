a9a11d1d9a0eadd8f646c76d31fb29857b236b35
Jan Merhar <merharjan@gmail.com>
2023-07-17 01:01:16 +0200

    Add atribute druzina in model models/revir.js

--- server/models/revir.js
+++ server/models/revir.js
@@ -7,6 +7,7 @@ const revirSchema = new Schema({
   ime: { type: String, required: true },
   // Dodaj subtype koordinata in maybe tudi revir sam po sebi
   koordinate: [{ type: koordinataSchema, required: true }],
+  druzina: { type: Schema.Types.ObjectId, ref: "Druzina", required: true },
 })
 
 module.exports = mongoose.model("Revir", revirSchema, "Revirji")
