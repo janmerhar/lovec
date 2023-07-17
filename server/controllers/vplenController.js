56ff71695fe14614550701e260d57715e67552ea
Jan Merhar <merharjan@gmail.com>
2023-07-17 14:32:45 +0200

    Add postVplen in controllers/vplenController.js

--- server/controllers/vplenController.js
+++ server/controllers/vplenController.js
@@ -1,17 +1,34 @@
+const Vplen = require("../entities/Vplen")
+const UporabnikFactory = require("../entities/UserRoles/UporabnikFactory")
+
 // Tukaj imam infinite scroll, ki mi vraca po zadnjih n vplenov
-exports.getVpleni = (req, res, next) => {
+exports.getVpleni = async (req, res, next) => {
   console.log("getUser")
   res.send("getUser")
 }
 
 // Tukaj dobim samo podatke o enem vplenu
 // ce jih se nisem prej vseh pridobil
-exports.getVplen = (req, res, next) => {
+exports.getVplen = async (req, res, next) => {
   console.log("getUser")
   res.send("getUser")
 }
 
-exports.postVplen = (req, res, next) => {
-  console.log("getUser")
-  res.send("getUser")
+exports.postVplen = async (req, res, next) => {
+  try {
+    const { zival, teza, datum, bolezni } = req.body
+    const { uporabnikId } = await UporabnikFactory.JWTpayload(req)
+
+    const result = await Vplen.vnesiVplen(
+      uporabnikId,
+      zival,
+      teza,
+      datum,
+      bolezni
+    )
+
+    res.send(result)
+  } catch (error) {
+    next(error)
+  }
 }
