1799c9398ff46d660b348e5d2f975087238b1b21
Jan Merhar <merharjan@gmail.com>
2023-07-20 00:49:54 +0200

    Implement ReponseBuilder in controllers/revirController.js

--- server/controllers/revirController.js
+++ server/controllers/revirController.js
@@ -1,10 +1,11 @@
 const Revir = require("../entities/Revir")
+const ResponseBuilder = require("../util/ResponseBuilder")
 
 exports.getRevirji = async (req, res, next) => {
   try {
     const result = await Revir.fetchRevirji()
 
-    res.send(result)
+    res.send(ResponseBuilder.success(result))
   } catch (error) {
     next(error)
   }
