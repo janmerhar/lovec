54f2e4e8cdf94e676e38f5a83772dcc1d930b006
Jan Merhar <merharjan@gmail.com>
2023-07-19 14:48:32 +0200

    Remove QR code from views/TabIzkaznica.vue

--- app/src/views/TabIzkaznica.vue
+++ app/src/views/TabIzkaznica.vue
@@ -6,36 +6,23 @@
       <grid-izkaznica :profile_picture="profile_picture"></grid-izkaznica>
 
       <ion-button expand="full">Odjava iz aplikacije</ion-button>
-      <h1 class="ion-text-center">QR koda</h1>
-      <qrcode-vue
-        :value="qr_value"
-        :size="1000"
-        level="H"
-        style="width: 100%; height: auto"
-      />
     </ion-content>
   </ion-page>
 </template>
 
 <script>
 import { IonPage, IonContent, IonButton } from "@ionic/vue"
-import QrcodeVue from "qrcode.vue"
 import GridIzkaznica from "@/components/izkaznica/GridIzkaznica.vue"
 
 export default {
   components: {
     IonPage,
-    QrcodeVue,
     IonContent,
     GridIzkaznica,
     IonButton,
   },
   data() {
-    return {
-      profile_picture:
-        "https://cdna.artstation.com/p/assets/images/images/038/826/858/large/edward-munn-edm-asset.jpg?1624192416",
-      qr_value: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    }
+    return {}
   },
 }
 </script>
