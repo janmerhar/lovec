4cd95c76a2da981b767bcdd2bba7b85c2d408cd2
Jan Merhar <merharjan@gmail.com>
2023-07-19 19:00:53 +0200

    Add front end Dnevnik skeleton in entities/Vplen.js

---
+++ app/src/entities/Dnevnik.js
@@ -0,0 +1,21 @@
+export class Dnevnik {
+  // Ali naj dodam se mentorjevo ime???
+  // mogoce to dodam ze v Pinia store, ko se prirpavnik logina
+  constructor(id, pripravnikId, mentorId, delo, ure, opis) {
+    this.id = id
+    this.pripravnikId = pripravnikId
+    this.mentorId = mentorId
+    this.delo = delo
+    this.ure = ure
+    this.opis = opis
+  }
+
+  async spremeniStatusDnevnik(status) {}
+
+  //   Funkcije za mentorja
+  static async fetchDnevnikiMentor(datum) {}
+  static async fetchDnevnikiPripravnik(stran) {}
+
+  //   Funkcije za pripravnika
+  static async vnesiDnevnik(datum, ure, opis, delo) {}
+}
