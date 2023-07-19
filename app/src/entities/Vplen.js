f0dfbfb3c67d231767203490da0494aac333add7
Jan Merhar <merharjan@gmail.com>
2023-07-20 01:39:16 +0200

    Add Vplen.vnesiVplen() in entities/Vplen.js

--- app/src/entities/Vplen.js
+++ app/src/entities/Vplen.js
@@ -1,14 +1,39 @@
-export class Vplen {
-  constructor({ _id, koordinate, zival, teza, datum, bolezni }) {
+/* eslint-disable */
+import { Request } from "../util/Request"
+
+export class Vplen extends Request {
+  constructor({ _id, zival, teza, datum, bolezni }) {
     this.id = _id
-    this.koordinate = koordinate
     this.zival = zival
     this.teza = teza
     this.datum = datum
     this.bolezni = bolezni
   }
 
-  static async vnesiVplen(koordinate, zival, teza, datum, bolezni) {}
-  static async fetchVpleni() {}
-  static async fetchVplenDatum(datum) {}
+  static async vnesiVplen(zival, teza, datum, bolezni) {
+    const result = await this.axiosInstance.post("/vpleni/dodaj", {
+      zival,
+      teza,
+      datum,
+      bolezni,
+    })
+
+    return result.data
+  }
+
+  static async fetchVpleni(stran) {
+    const result = await this.axiosInstance.get(`/vpleni/${stran}`)
+
+    // result.data.data = result.data.data.map((vplen) => {
+    //   return {
+    //     datum: vplen.datum,
+    //     zivali: vplen.zivali.map(
+    //       (zival) => zival.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+    //     ),
+    //   }
+    // })
+
+    return result.data
+  }
+  // static async fetchVplenDatum(datum) {}
 }
