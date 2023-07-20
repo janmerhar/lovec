6e19bdf81b39c1c9ae088a00a56bf4f5b7d31981
Jan Merhar <merharjan@gmail.com>
2023-07-20 18:27:54 +0200

    Add Oprema.fetchUporabnikOprema() in entities/Oprema.js

--- app/src/entities/Oprema.js
+++ app/src/entities/Oprema.js
@@ -1,12 +1,23 @@
-export class Oprema {
-  constructor({ _id, naziv, tip, stanje }) {
+import { Request } from "../util/Request"
+
+export class Oprema extends Request {
+  constructor({ _id, naziv, tip, stanje, datum }) {
+    super()
     this.id = _id
     this.naziv = naziv
     this.tip = tip
     this.stanje = stanje
+    this.datum = datum
   }
 
-  static async fetchUporabnikOprema() {}
+  static async fetchUporabnikOprema() {
+    const result = await this.axiosInstance.get("/oprema/uporabnik")
+
+    result.data.data = result.data.data.map((oprema) => new Oprema(oprema))
+
+    return result.data
+  }
+
   static async vnesiOprema(naziv, tip, stanje) {}
   async izbrisiOprema() {}
 }
