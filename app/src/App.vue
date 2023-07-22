fd55605b3c3e311e8c0e012ffafc5590035dbf22
Jan Merhar <merharjan@gmail.com>
2023-07-22 06:53:17 +0200

    Add setBearer() in src/App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,6 +4,29 @@
   </ion-app>
 </template>
 
-<script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+<script>
+import { IonApp, IonRouterOutlet } from "@ionic/vue"
+
+import { defineComponent } from "vue"
+
+import { useUporabnikStore } from "@/stores/useUporabnikStore"
+
+export default defineComponent({
+  components: {
+    IonApp,
+    IonRouterOutlet,
+  },
+  methods: {
+    setBearer() {
+      // Reading JWT token from Pinia store on reload
+      const uporabnikStore = useUporabnikStore()
+
+      this.axios.defaults.headers.Authorization = `Bearer ${uporabnikStore.token}`
+      console.log("Token: ", this.axios.defaults.headers.Authorization)
+    },
+  },
+  async beforeMount() {
+    this.setBearer()
+  },
+})
 </script>
