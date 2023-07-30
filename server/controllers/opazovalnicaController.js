e4eff85d4ac6fa7a8ee3bf722c8e9bc4cd199088
Jan Merhar <merharjan@gmail.com>
2023-07-31 00:59:41 +0200

    Add postRezerviraj() in controllers/opazovalnicaController.js

--- server/controllers/opazovalnicaController.js
+++ server/controllers/opazovalnicaController.js
@@ -1,3 +1,9 @@
+const Opazovalnica = require("../entities/Opazovalnica")
+
+const UporabnikFactory = require("../entities/UserRoles/UporabnikFactory")
+
+const ResponseBuilder = require("../util/ResponseBuilder")
+
 // Vrnem vse podatke o neki opazovalnici
 // mogoce se samo omejim na dolocen casovni razpon obiskov
 exports.getOpazovalnica = (req, res, next) => {
@@ -16,3 +22,25 @@ exports.getObiski = (req, res, next) => {
   console.log("getUser")
   res.send("getUser")
 }
+
+exports.postRezerviraj = async (req, res, next) => {
+  try {
+    const { uporabnikId } = await UporabnikFactory.JWTpayload(req)
+    const { id } = req.params
+
+    const { zacetek, konec } = req.body
+
+    const result = await Opazovalnica.rezervirajOpazovalnico(
+      uporabnikId,
+      id,
+      zacetek,
+      konec
+    )
+
+    res.send(ResponseBuilder.success(result))
+    // res.send("rezervacija")
+  } catch (error) {
+    next(error)
+  }
+}
+
