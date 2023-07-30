6752579c88c6b2422a8e56d0cdaef275e1cff7d0
Jan Merhar <merharjan@gmail.com>
2023-07-31 00:47:05 +0200

    Add Opazovalnica.fetchOpazovalnice() in entities/Opazovalnica.js

--- server/entities/Opazovalnica.js
+++ server/entities/Opazovalnica.js
@@ -7,7 +7,56 @@ module.exports = class Opazovalnica {
     this.obiski = obiski
   }
 
-  static async fetchOpazovalnica(id) {}
-  static async fetchOpazovalnice() {}
-  static async fetchObiski(id, datum) {}
+  static async fetchOpazovalnica(id) {
+    const opazovalnica = await OpazovalnicaModel.findById(id)
+    console.log(opazovalnica)
+    // return opazovalnica
+  }
+
+  // TODO
+  // preveri s polnimi podatki
+  // mogoce samo naredim, da ima vsaka opazovalnice ze dane danasnje podatke
+  static async fetchOpazovalnice() {
+    const today = new Date()
+    today.setHours(0, 0, 0, 0) // Set the time to the start of the day
+
+    const pipeline = [
+      {
+        $addFields: {
+          obiski: {
+            $filter: {
+              input: "$obiski",
+              cond: {
+                $gte: ["$$this.zacetek", today],
+              },
+            },
+          },
+        },
+      },
+      {
+        $addFields: {
+          "obiski.konec": {
+            $cond: {
+              if: { $eq: [{ $size: "$obiski" }, 1] },
+              then: "$$REMOVE", // If there's only one element, remove the 'konec' field
+              else: "$obiski.konec",
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          koordinate: 1,
+          obiski: 1,
+        },
+      },
+    ]
+
+    // Assuming 'Opazovalnica' is your model name
+    const result = OpazovalnicaModel.aggregate(pipeline).exec()
+
+    return result
+  }
+
+  async fetchObiski(datum) {}
 }
