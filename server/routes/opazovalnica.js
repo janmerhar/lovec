693264efb0dd2b530641363668d9e96bb94d9fbf
Jan Merhar <merharjan@gmail.com>
2023-09-10 19:10:36 +0200

    Add rezerviraj route in routes/opazovalnica.js

--- server/routes/opazovalnica.js
+++ server/routes/opazovalnica.js
@@ -6,7 +6,8 @@ const router = express.Router()
 const opazovalnicaController = require("../controllers/opazovalnicaController")
 
 router.get("/:id", opazovalnicaController.getOpazovalnica)
-router.get("/opazovalnice", opazovalnicaController.getOpazovalnice)
+router.get("/", opazovalnicaController.getOpazovalnice)
 router.get("/:id/obiski/:datum", opazovalnicaController.getObiski)
+router.post("/:id/rezerviraj", opazovalnicaController.postRezerviraj)
 
 module.exports = router
