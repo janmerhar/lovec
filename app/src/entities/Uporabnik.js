b3f63dfa0b6067cb31cf557f1c7b252e385dce0c
Jan Merhar <merharjan@gmail.com>
2023-09-10 18:58:38 +0200

    Add Uporabnik.isJWTvalid() in Uporabnik.js

--- app/src/entities/Uporabnik.js
+++ app/src/entities/Uporabnik.js
@@ -1,4 +1,5 @@
 const { Request } = require("../util/Request")
+import jwt_decode from "jwt-decode"
 
 export class Uporabnik extends Request {
   // mogoce tebi dam tudi konstruktor
@@ -35,18 +36,26 @@ export class Uporabnik extends Request {
   //
 
   isJWTvalid(token) {
-    if (!token) return false
+    if (!token) return -1 // Token not provided or empty
 
     try {
       const decodedToken = jwt_decode(token)
-      const currentTime = Date.now() / 1000
+      const currentTime = Math.floor(Date.now() / 1000) // Convert current time to seconds
+      const expirationTime = decodedToken.exp
 
-      return decodedToken.exp > currentTime
+      if (expirationTime > currentTime) {
+        return expirationTime - currentTime // Remaining time in seconds
+      } else {
+        return 0 // Token has expired
+      }
     } catch (error) {
-      return false
+      // Failed to decode the token (invalid token format, etc.)
+      return -1
     }
   }
 
+  // async JWTrefresh() {}
+  // JWTpayload() {}
 
   static async login(email, geslo) {
     const uporabnik = await this.axiosInstance.post("/login", { email, geslo })
@@ -56,4 +65,10 @@ export class Uporabnik extends Request {
     return uporabnik.data
   }
 
+  // static async register(ime, priimek, slika, rojstniDatum, email, geslo) {}
+  // static async fetchUporabnik(uporabnikId) {}
+
+  // Ti periodicno preverjas, ali je token se veljaven
+  // in ga po potrebi osvezi
+  // static async refreshToken() {}//
 }
