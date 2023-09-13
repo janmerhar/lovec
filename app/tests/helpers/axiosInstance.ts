9a999093384e95ad3b1987b21f793fb28f17d138
Jan Merhar <merharjan@gmail.com>
2023-09-13 16:22:19 +0200

    Fix incorrect export axiosInstance.ts #21

--- app/tests/helpers/axiosInstance.ts
+++ app/tests/helpers/axiosInstance.ts
@@ -1,11 +1,9 @@
 import axios from "axios"
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: "http://localhost:1234",
   headers: {
     "Content-Type": "application/json",
     Authorization: "",
   },
 })
-
-export axiosInstance
