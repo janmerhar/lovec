eae2a52e624ade1cc8413315e456137d1d6ce014
Jan Merhar <merharjan@gmail.com>
2023-09-13 13:37:28 +0200

    Add axiosInstance for testing #7

---
+++ app/tests/unit/entities/axiosInstance.ts
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:1234",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "",
+  },
+})
+
+export default axiosInstance
