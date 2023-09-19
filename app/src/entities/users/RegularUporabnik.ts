3667258ec61e0c576ef46158a36a518eff0c11e8
Jan Merhar <merharjan@gmail.com>
2023-09-20 01:12:29 +0200

    Update for noImplicitAny in RegularUporabnik.ts #25

--- app/src/entities/users/RegularUporabnik.ts
+++ app/src/entities/users/RegularUporabnik.ts
@@ -8,7 +8,25 @@ export class RegularUporabnik extends Uporabnik {
 
   constructor(
     axioInstance: AxiosInstance,
-    { _id, ime, priimek, slika, rojstniDatum, email, token, druzina }
+    {
+      _id,
+      ime,
+      priimek,
+      slika,
+      rojstniDatum,
+      email,
+      token,
+      druzina,
+    }: {
+      _id: string
+      ime: string
+      priimek: string
+      slika: string
+      rojstniDatum: Date
+      email: string
+      token: string
+      druzina: string | null
+    }
   ) {
     super(axioInstance, { _id, ime, priimek, email, token })
 
