9408cc7227eb43c58299267ea6da1179b7d0f7c8
Jan Merhar <merharjan@gmail.com>
2023-09-20 01:12:20 +0200

    Update for noImplicitAny in Pripravnik.ts #25

--- app/src/entities/users/Pripravnik.ts
+++ app/src/entities/users/Pripravnik.ts
@@ -6,7 +6,27 @@ export class Pripravnik extends RegularUporabnik {
 
   constructor(
     axiosInstance: AxiosInstance,
-    { _id, ime, priimek, slika, rojstniDatum, email, token, druzina, mentor }
+    {
+      _id,
+      ime,
+      priimek,
+      slika,
+      rojstniDatum,
+      email,
+      token,
+      druzina,
+      mentor,
+    }: {
+      _id: string
+      ime: string
+      priimek: string
+      slika: string
+      rojstniDatum: Date
+      email: string
+      token: string
+      druzina: string | null
+      mentor: string | null
+    }
   ) {
     super(axiosInstance, {
       _id,
