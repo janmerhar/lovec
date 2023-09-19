ae654299c8552c982c306f10ec3b791a3a0473aa
Jan Merhar <merharjan@gmail.com>
2023-09-19 23:48:59 +0200

    Add Lovec.spec.ts #24

---
+++ app/tests/unit/entities/users/Lovec.spec.ts
@@ -0,0 +1,32 @@
+import { Lovec } from "@/entities/users/Lovec"
+import { axiosInstance } from "../../../helpers/axiosInstance"
+
+describe("Lovec class", () => {
+  describe("constructor", () => {
+    it("constructs lovec", () => {
+      const lovecData = {
+        _id: "1",
+        ime: "ime",
+        priimek: "priimek",
+        slika: "slika",
+        rojstniDatum: "rojstniDatum",
+        email: "email",
+        token: "token",
+        druzina: "druzina",
+        pripravniki: "pripravniki",
+      }
+
+      const lovec = new Lovec(axiosInstance, lovecData)
+
+      expect(lovec.id).toBe(lovecData._id)
+      expect(lovec.ime).toBe(lovecData.ime)
+      expect(lovec.priimek).toBe(lovecData.priimek)
+      expect(lovec.slika).toBe(lovecData.slika)
+      expect(lovec.rojstniDatum).toBe(lovecData.rojstniDatum)
+      expect(lovec.email).toBe(lovecData.email)
+      expect(lovec.jwt.token).toBe(lovecData.token)
+      expect(lovec.druzina).toBe(lovecData.druzina)
+      expect(lovec.pripravniki).toBe(lovecData.pripravniki)
+    })
+  })
+})
