7d06717aff4181c1307ec45a15518e548966fd33
Jan Merhar <merharjan@gmail.com>
2023-09-20 01:12:04 +0200

    Update for noImplicitAny in Admin.ts #25

--- app/src/entities/users/Admin.ts
+++ app/src/entities/users/Admin.ts
@@ -4,7 +4,19 @@ import { AxiosInstance } from "axios"
 export class Admin extends Uporabnik {
   constructor(
     axiosInstance: AxiosInstance,
-    { _id, ime, priimek, email, token }
+    {
+      _id,
+      ime,
+      priimek,
+      email,
+      token,
+    }: {
+      _id: string
+      ime: string
+      priimek: string
+      email: string
+      token: string
+    }
   ) {
     super(axiosInstance, { _id, ime, priimek, email, token })
   }
