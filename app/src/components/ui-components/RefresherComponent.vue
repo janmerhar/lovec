ee788d6d1a7111259f4e2ebf76c0b632b386bb04
Jan Merhar <merharjan@gmail.com>
2023-10-06 16:56:04 +0200

    Add RefresherComponent.vue #31

---
+++ app/src/components/ui-components/RefresherComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <ion-refresher slot="fixed" @ionRefresh="handleRefresh($event)">
+    <ion-refresher-content></ion-refresher-content>
+  </ion-refresher>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+import { IonRefresher, IonRefresherContent } from "@ionic/vue"
+
+export default defineComponent({
+  components: {
+    IonRefresher,
+    IonRefresherContent,
+  },
+  props: {
+    refresh: {
+      type: Function,
+      required: true,
+    },
+  },
+  emits: ["refresh"],
+  methods: {
+    handleRefresh(event: CustomEvent) {
+      // Verjetno lahko dodam se kaksen delay prej, bom videl
+      this.refresh(event)
+    },
+  },
+})
+</script>
