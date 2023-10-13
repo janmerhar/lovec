fa9ad9f7d57b964ac574f73eba4e641d5654cc27
Jan Merhar <merharjan@gmail.com>
2023-10-13 14:48:14 +0200

    Add ResponseBuilder.ts #39

--- server/utils/ResponseBuilder.ts
+++ server/utils/ResponseBuilder.ts
@@ -1,23 +1,27 @@
 module.exports = class ResponseBuilder {
-  constructor(status, data, message) {
+  status: number
+  data: any
+  message: string
+
+  constructor(status: number, data: any, message: string) {
     this.status = status
     this.data = data
     this.message = message
   }
 
-  static success(data) {
+  static success(data: any) {
     return new ResponseBuilder(200, data, "Success")
   }
 
-  static error(message) {
+  static error(message: string) {
     return new ResponseBuilder(500, null, message)
   }
 
-  static unauthorized(message) {
+  static unauthorized(message: string) {
     return new ResponseBuilder(400, null, message)
   }
 
-  static token(token) {
+  static token(token: string) {
     return new ResponseBuilder(200, token, "Success")
   }
 }
