99930a0646c83aaff84a5c1c436eea3cca391ddf
Jan Merhar <merharjan@gmail.com>
2023-10-13 16:13:18 +0200

    Add vplenModel.ts #40

---
+++ server/models/vplenModel.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document, Model } from "mongoose"
+
+export interface IVplen extends Document {
+  uporabnik: Schema.Types.ObjectId
+  koordinate: [number, number]
+  zival: string
+  teza: number
+  datum: Date
+  bolezni?: string[]
+}
+
+const vplenSchema = new Schema<IVplen>({
+  uporabnik: { type: Schema.Types.ObjectId, ref: "Uporabnik", required: true },
+  koordinate: {
+    type: [Number],
+    required: true,
+  },
+  zival: {
+    type: String,
+    required: true,
+  },
+  teza: { type: Number, required: true },
+  datum: { type: Date, required: true },
+  bolezni: {
+    type: [String],
+    required: false,
+  },
+})
+
+const VplenModel: Model<IVplen> = mongoose.model("Vplen", vplenSchema, "Vpleni")
+
+export default VplenModel
