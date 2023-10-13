1cd83a367ca0f1acd9e1392ddb77c28820624973
Jan Merhar <merharjan@gmail.com>
2023-10-13 14:47:16 +0200

    Add ErrorHandler.ts #39

--- server/utils/ErrorHandler.ts
+++ server/utils/ErrorHandler.ts
@@ -1,17 +1,20 @@
-module.exports = (err, req, res, next) => {
-  console.log("Middleware Error Handling")
+export default (err: any, req: any, res: any, next: any) => {
+  console.log()
+  console.log("\x1b[41mMiddleware Error Handling\x1b[0m")
 
   const errStatus = err.statusCode || 500
   let errMsg = "Prišlo je do napake"
 
   if (typeof err === "string") {
     errMsg = err
+    console.log(errMsg)
   } else if (typeof err === "object") {
     errMsg = err.message
 
     // Printing out message stack
     console.log(err.stack)
   }
+  console.log()
 
   res.status(errStatus).json({
     success: false,
