604d9a389b0f0f9cae9ae1502ef8546c7bfb0d04
Jan Merhar <merharjan@gmail.com>
2023-10-13 18:10:59 +0200

    Remove koordinataSchema.js #40

--- server/models/koordinataSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-const { Schema } = mongoose
-
-const koordinataSchema = new Schema({
-  latitude: { type: Number, required: true },
-  longitude: { type: Number, required: true },
-})
-
-module.exports = koordinataSchema
