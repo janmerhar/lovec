078e50308b9727d993bf95ebcb59afc63d0e6556
Jan Merhar <merharjan@gmail.com>
2023-10-14 13:19:05 +0200

    Add Uporabnik.ts #41

--- server/entities/Uporabnik.ts
+++ server/entities/Uporabnik.ts
@@ -1,23 +1,40 @@
-const UporabnikModel = require("../../models/uporabnikModel")
-const bcrypt = require("bcrypt")
+import UporabnikModel from "@models/uporabnikModel"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { Request } from "express"
 
-const jwt = require("jsonwebtoken")
-module.exports = class UporabnikFactory {
-  // Tebe dam v factory
-  static async login(email, password) {
+//
+// TODO
+// add return datatypes
+// but first, create those types
+
+export default class Uporabnik {
+  static async login(email: string, password: string) {
     const uporabnik = await UporabnikModel.findOne({ email }).exec()
+
+    if (uporabnik == null) {
+      return null
+    }
 
     const geslo_verify = await bcrypt.compare(password, uporabnik.hash)
 
     if (geslo_verify) {
+      // TODO
+      // @ts-ignore
       const token = await this.JWTcreate({
         uporabnikId: uporabnik._id.toString(),
         role: uporabnik.role,
       })
 
-      let returnUporabnik = await UporabnikFactory.fetchUporabnik(uporabnik._id)
+      let returnUporabnik = await Uporabnik.fetchUporabnik(uporabnik._id)
+
+      if (returnUporabnik == null) {
+        return null
+      }
 
       returnUporabnik = returnUporabnik.toObject()
+      // TODO
+      // @ts-ignore
       returnUporabnik.refresh_token = undefined
 
       return {
@@ -28,7 +45,14 @@ module.exports = class UporabnikFactory {
     return null
   }
 
-  static async register(ime, priimek, slika, rojstniDatum, email, geslo) {
+  static async register(
+    ime: string,
+    priimek: string,
+    slika: string,
+    rojstniDatum: string,
+    email: string,
+    geslo: string
+  ) {
     const salt = await bcrypt.genSalt(10)
     const geslo_hash = await bcrypt.hash(geslo, salt)
 
@@ -52,12 +76,14 @@ module.exports = class UporabnikFactory {
     return true
   }
 
-  static async fetchUporabnik(uporabnikId) {
+  static async fetchUporabnik(uporabnikId: string) {
     const result = await UporabnikModel.findById(uporabnikId)
       .populate("mentor", "_id ime priimek ")
       .populate("druzina", "_id ime")
       .exec()
 
+    // TODO
+    // @ts-ignore
     result.hash = undefined
 
     return result
@@ -66,17 +92,23 @@ module.exports = class UporabnikFactory {
   //
   // JWT
   //
-  static async JWTcreate(payload) {
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" })
+  static async JWTcreate(payload: string) {
+    const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+      expiresIn: "1h",
+    })
 
     return token
   }
 
-  static async JWTverify(req) {
+  static async JWTverify(req: Request) {
     try {
+      if (!req.headers.authorization) {
+        return false
+      }
+
       const token = req.headers.authorization.split(" ")[1]
 
-      const payload = jwt.verify(token, process.env.JWT_SECRET)
+      const payload = jwt.verify(token, process.env.JWT_SECRET as string)
 
       return true
     } catch (error) {
@@ -84,14 +116,22 @@ module.exports = class UporabnikFactory {
     }
   }
 
-  static async JWTpayload(req) {
+  static async JWTpayload(req: Request) {
     try {
+      if (!req.headers.authorization) {
+        throw new Error("Napaka pri preverjanju JWT")
+      }
+
       const token = req.headers.authorization.split(" ")[1]
 
-      const payload = jwt.verify(token, process.env.JWT_SECRET)
+      const payload = jwt.verify(token, process.env.JWT_SECRET as string)
 
       return {
+        // TODO
+        // @ts-ignore
         uporabnikId: payload.uporabnikId,
+        // TODO
+        // @ts-ignore
         role: payload.role,
       }
     } catch (error) {
@@ -99,5 +139,5 @@ module.exports = class UporabnikFactory {
     }
   }
 
-  static async JWTrefresh(req) {}
+  static async JWTrefresh(req: Request) {}
 }
