6f28a07c50d33cefdabe6c1c0ddbac736cd92cea
Jan Merhar <merharjan@gmail.com>
2023-12-10 19:24:52 +0100

    Add jagaModel.ts

---
+++ server/models/jagaModel.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document, Model } from "mongoose"
+import type { IJaga } from "@shared/types"
+
+const jagaSchema = new Schema<IJaga>({
+  organizator: {
+    type: Schema.Types.ObjectId,
+    ref: "Uporabnik",
+    required: true,
+  },
+  udelezeni: [
+    { type: Schema.Types.ObjectId, ref: "Uporabnik", required: true },
+  ],
+  maxUdelezeni: {
+    type: Number,
+    required: true,
+  },
+  lokacija: {
+    type: [[Number]],
+    required: true,
+  },
+  zacetek: {
+    type: Date,
+    required: true,
+  },
+})
+
+const JagaModel: Model<IJaga> = mongoose.model("Jaga", jagaSchema, "Jage")
+
+export default JagaModel
+export type JagaDocument = IJaga & Document
