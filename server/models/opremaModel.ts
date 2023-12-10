74c1480019a58fd7a613765f91c8cfe70410f462
Jan Merhar <merharjan@gmail.com>
2023-12-10 21:09:55 +0100

    Update opremaModel.ts with enum

--- server/models/opremaModel.ts
+++ server/models/opremaModel.ts
@@ -1,12 +1,12 @@
-import mongoose, { Schema, Document, ObjectId } from "mongoose"
-import { IOprema } from "@shared/types"
+import mongoose, { Schema, Document } from "mongoose"
+import type { IOprema } from "@shared/types"
+import { opremaTipDomain } from "@shared/types"
 
 const opremaSchema = new Schema<IOprema>({
   lastnik: { type: Schema.Types.ObjectId, ref: "Uporabnik", required: false },
   naziv: { type: String, required: true },
-  tip: { type: String, required: true },
+  tip: { type: String, enum: opremaTipDomain, required: true },
   stanje: { type: String, required: true },
-  // set the field `datum` to current date (time)
   datum: { type: Date, default: Date.now },
 })
 
