585e9225a0d57f835bed98f77997f79fc0523155
Jan Merhar <merharjan@gmail.com>
2023-12-12 21:07:08 +0100

    Add revirController.ts

--- server/controllers/revirController.ts
+++ server/controllers/revirController.ts
@@ -1,18 +1,66 @@
 import Revir from "@entities/Revir"
 import ResponseBuilder from "@utils/ResponseBuilder"
 
-import { Request, Response, NextFunction, RequestHandler } from "express"
+import {
+  Body,
+  Get,
+  Post,
+  JsonController,
+  Req,
+  UseBefore,
+  Delete,
+  Param,
+  Patch,
+  Put,
+} from "routing-controllers"
 
-export const getRevirji: RequestHandler = async (
-  _req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
+import { InsertRevirDTO } from "./dto/revir/insert-revir.dto"
+
+import { authUser } from "middleware/authUser"
+
+@JsonController("/revirji")
+export class RevirController {
+  @Post("/")
+  @UseBefore(authUser("admin"))
+  async postRevir(@Body() revir: InsertRevirDTO) {
+    const result = await Revir.vnesiRevir(
+      revir.ime,
+      revir.koordinate,
+      revir.druzina
+    )
+
+    return ResponseBuilder.success(result)
+  }
+
+  @Get("/")
+  @UseBefore(authUser("pripravnik", "lovec", "admin"))
+  async getRevirji() {
     const result = await Revir.fetchRevirji()
 
-    res.send(ResponseBuilder.success(result))
-  } catch (error) {
-    next(error)
+    return ResponseBuilder.success(result)
+  }
+
+  @Delete("/:id")
+  @UseBefore(authUser("admin"))
+  async deleteRevir(@Param("id") id: string) {
+    const result = await Revir.odstraniRevir(id)
+
+    return result ? ResponseBuilder.success(result) : ResponseBuilder.notfound()
+  }
+
+  @Patch("/:id")
+  @UseBefore(authUser("admin"))
+  async updateRevir(@Param("id") id: string, @Body() revir: InsertRevirDTO) {
+    console.log(id)
+    console.log(revir)
+
+    const result = await Revir.updateRevir(
+      id,
+      revir.ime,
+      revir.koordinate,
+      revir.druzina
+    )
+
+    return result ? ResponseBuilder.success(result) : ResponseBuilder.notfound()
   }
 }
--- server/routes/revirRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const revir = express.Router()
-
-import { getRevirji } from "@controllers/revirController"
-
-revir.get("/", getRevirji)
-
-export default revir
