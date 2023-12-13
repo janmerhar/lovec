d20bfb6e797fe05a5b33bb11a6b217f36cb3a78a
Jan Merhar <merharjan@gmail.com>
2023-12-13 19:13:53 +0100

    Update Uporabnik.ts

--- server/entities/Uporabnik.ts
+++ server/entities/Uporabnik.ts
@@ -2,6 +2,8 @@ import UporabnikModel from "@models/uporabnikModel"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import { Request } from "express"
+import { DruzinaDetails } from "./Druzina"
+import type { IDruzinaDetails, IUporabnikDetails } from "@shared/types"
 
 export class UporabnikDetails {
   id: string
@@ -25,7 +27,54 @@ export class UporabnikDetails {
   }
 }
 
-export default class Uporabnik {
+export class UserProfile {}
+
+// manjka
+// mentordetails
+// pripravnikdetails
+// admin details
+export default class Uporabnik<M = string, D = string> {
+  id: string
+  ime: string
+  priimek: string
+  slika: string
+  rojstniDatum: string
+  email: string
+  hash: string
+  role: string
+  mentor: M | null
+  pripravniki: string[]
+  druzina: D | null
+  refresh_token: string | null
+
+  constructor(
+    id: string,
+    ime: string,
+    priimek: string,
+    slika: string,
+    rojstniDatum: string,
+    email: string,
+    hash: string,
+    role: string,
+    mentor: M | null,
+    pripravniki: string[],
+    druzina: D | null,
+    refresh_token: string | null
+  ) {
+    this.id = id
+    this.ime = ime
+    this.priimek = priimek
+    this.slika = slika
+    this.rojstniDatum = rojstniDatum
+    this.email = email
+    this.hash = hash
+    this.role = role
+    this.mentor = mentor
+    this.pripravniki = pripravniki
+    this.druzina = druzina
+    this.refresh_token = refresh_token
+  }
+
   static async login(email: string, password: string) {
     const uporabnik = await UporabnikModel.findOne({ email }).exec()
 
@@ -51,7 +100,6 @@ export default class Uporabnik {
         return null
       }
 
-      returnUporabnik = returnUporabnik.toObject()
       // TODO
       // @ts-ignore
       returnUporabnik.refresh_token = undefined
@@ -95,17 +143,45 @@ export default class Uporabnik {
     return true
   }
 
-  static async fetchUporabnik(uporabnikId: string) {
+  static async fetchUporabnik(
+    uporabnikId: string
+  ): Promise<Uporabnik<UporabnikDetails, DruzinaDetails> | null> {
     const result = await UporabnikModel.findById(uporabnikId)
-      .populate("mentor", "_id ime priimek ")
-      .populate("druzina", "_id ime")
+      .populate<{ mentor: IUporabnikDetails }>(
+        "mentor",
+        "_id ime priimek slika role"
+      )
+      .populate<{ druzina: IDruzinaDetails }>("druzina", "_id ime")
       .exec()
 
-    // TODO
-    // @ts-ignore
-    result.hash = undefined
+    if (result == null) {
+      return null
+    }
 
-    return result
+    return new Uporabnik<UporabnikDetails, DruzinaDetails>(
+      result._id.toString(),
+      result.ime,
+      result.priimek,
+      result.slika,
+      result.rojstniDatum ? result.rojstniDatum.toISOString() : "",
+      result.email,
+      result.hash,
+      result.role,
+      result.mentor
+        ? new UporabnikDetails(
+            result.mentor._id.toString(),
+            result.mentor.ime,
+            result.mentor.priimek,
+            result.mentor.slika,
+            result.mentor.role
+          )
+        : null,
+      result.pripravniki ? result.pripravniki.map((id) => id.toString()) : [],
+      result.druzina
+        ? new DruzinaDetails(result.druzina._id.toString(), result.druzina.ime)
+        : null,
+      result.refresh_token
+    )
   }
 
   static async odstraniClanstvoDruzine(uporabnikId: string): Promise<boolean> {
