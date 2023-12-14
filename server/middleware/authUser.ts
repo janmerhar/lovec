e6e10f6a4ec026bc3d248409b369d17e4bb9fe83
Jan Merhar <merharjan@gmail.com>
2023-12-14 16:22:01 +0100

    Update authUser.ts

--- server/middleware/authUser.ts
+++ server/middleware/authUser.ts
@@ -6,7 +6,6 @@ import ResponseBuilder from "@utils/ResponseBuilder"
 export const authUser = (...roles: string[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const { role } = await Uporabnik.JWTpayload(req)
-    console.log(role)
 
     if (roles.indexOf(role) === -1) {
       return res.status(401).send(ResponseBuilder.unauthorized("Unauthorized"))
