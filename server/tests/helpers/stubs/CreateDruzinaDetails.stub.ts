7116fb995ba874d613c789a2da650dddfa2c66cf
Jan Merhar <merharjan@gmail.com>
2024-02-11 00:05:28 +0100

    Update CreateDruzinaDetails.stub.ts

--- server/tests/helpers/stubs/CreateDruzinaDetails.stub.ts
+++ server/tests/helpers/stubs/CreateDruzinaDetails.stub.ts
@@ -5,5 +5,7 @@ export const CreateDruzinaDetailsStub = (): IDruzinaDetails => {
   return {
     _id: new mongoose.Types.ObjectId("65c80054921864fcfd09604f"),
     ime: "druzinaIme",
+    revirjiCount: 1,
+    claniCount: 2,
   }
 }
