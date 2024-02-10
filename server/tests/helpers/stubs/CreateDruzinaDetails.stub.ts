d12bc3a8e98428536c5e69250528c16343b20afb
Jan Merhar <merharjan@gmail.com>
2024-02-11 00:03:02 +0100

    Add CreateDruzinaDetails.stub.ts

---
+++ server/tests/helpers/stubs/CreateDruzinaDetails.stub.ts
@@ -0,0 +1,9 @@
+import type { IDruzinaDetails } from "~/shared/types"
+import mongoose from "mongoose"
+
+export const CreateDruzinaDetailsStub = (): IDruzinaDetails => {
+  return {
+    _id: new mongoose.Types.ObjectId("65c80054921864fcfd09604f"),
+    ime: "druzinaIme",
+  }
+}
