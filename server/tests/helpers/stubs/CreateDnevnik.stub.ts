6af6b5f13b8c72939cfab24933924a9e08c1d6ba
Jan Merhar <merharjan@gmail.com>
2024-02-10 16:18:10 +0100

    Update CreateDnevnik.stub.ts

--- server/tests/helpers/stubs/CreateDnevnik.stub.ts
+++ server/tests/helpers/stubs/CreateDnevnik.stub.ts
@@ -1,9 +1,10 @@
 import type { IDnevnik } from "@shared/types"
+import mongoose from "mongoose"
 
-export const CreateDnevnikStub = (): IDnevnik<string, string, string> => ({
-  _id: "1",
-  pripravnik: "1",
-  mentor: "1",
+export const CreateDnevnikStub = (): IDnevnik => ({
+  _id: new mongoose.Types.ObjectId("65c78ca1a7f1d997defbe169"),
+  pripravnik: new mongoose.Types.ObjectId("65c78f41a7f1d997defbe16a"),
+  mentor: new mongoose.Types.ObjectId("65c78f64a7f1d997defbe16b"),
   status: "drugo",
   datum: new Date("2024-02-10T14:23:13.888Z"),
   ure: 12,
