9c9b0df58dba570282e6e933a6d9f0a857197781
Jan Merhar <merharjan@gmail.com>
2024-02-11 17:07:24 +0100

    Add Opazovalnica.spec.ts

---
+++ server/tests/unit/Opazovalnica.spec.ts
@@ -0,0 +1,34 @@
+import Opazovalnica from "@entities/Opazovalnica"
+import { IOpazovalnica } from "@shared/types"
+
+describe("Opazovalnica", () => {
+  describe("constructor", () => {
+    it.todo("should create an instance of Opazovalnica")
+  })
+
+  describe("fetchOpazovalnica", () => {
+    it.todo("should fetch an instance of Opazovalnica")
+
+    it.todo("should return null if no instance is found")
+  })
+
+  describe("fetchOpazovalnice", () => {
+    it.todo("should fetch all instances of Opazovalnica")
+
+    it.todo("should return an empty array if no instances are found")
+  })
+
+  describe("addOpazovalnica", () => {
+    it.todo("should add an instance of Opazovalnica")
+  })
+
+  describe("updateOpazovalnica", () => {
+    it.todo("should update an instance of Opazovalnica")
+    it.todo("should return null if no instance is found")
+  })
+
+  describe("deleteOpazovalnica", () => {
+    it.todo("should delete an instance of Opazovalnica")
+    it.todo("should return true if no instance is found")
+  })
+})
