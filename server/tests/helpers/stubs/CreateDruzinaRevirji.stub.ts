062adc4e5dd6bc9cd79c2699067bfe9da01726ff
Jan Merhar <merharjan@gmail.com>
2024-02-11 13:13:02 +0100

    Update CreateDruznaRevirji.stub.ts

--- server/tests/helpers/stubs/CreateDruzinaRevirji.stub.ts
+++ server/tests/helpers/stubs/CreateDruzinaRevirji.stub.ts
@@ -1,9 +1,9 @@
-import type { IDruzina, IRevir } from "@shared/types"
+import type { IDruzina, IRevirDetails } from "@shared/types"
 import mongoose from "mongoose"
 
 export const CreateDruzinaRevirjiStub = (): IDruzina<
   mongoose.Types.ObjectId,
-  IRevir,
+  IRevirDetails,
   mongoose.Types.ObjectId
 > => ({
   _id: new mongoose.Types.ObjectId("65c80054921864fcfd09604f"),
@@ -13,7 +13,6 @@ export const CreateDruzinaRevirjiStub = (): IDruzina<
       _id: new mongoose.Types.ObjectId("65c80054921864fcfd09604f"),
       ime: "revirIme",
       koordinate: [[1, 2]],
-      druzina: new mongoose.Types.ObjectId("65c80054921864fcfd09604f"),
     },
   ],
   clani: [new mongoose.Types.ObjectId("65c80054921864fcfd09604f")],
