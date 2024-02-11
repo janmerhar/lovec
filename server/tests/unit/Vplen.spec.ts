0c14164aa891d03fbd485eb84a388c3fb9906c13
Jan Merhar <merharjan@gmail.com>
2024-02-11 17:38:14 +0100

    Add Vplen.spec.ts

---
+++ server/tests/unit/Vplen.spec.ts
@@ -0,0 +1,27 @@
+import VplenModel from "@models/vplenModel"
+import type { IVplen, IVplenDetails } from "@shared/types"
+import mongoose from "mongoose"
+
+describe("Vplen", () => {
+  describe("constructor", () => {
+    it.todo("should create a new instance of Vplen")
+  })
+
+  describe("fetchVpleni", () => {
+    it.todo(
+      "should fetch an array of Vpleni for a specific user and page number"
+    )
+  })
+
+  describe("fetchVpleniDatun", () => {
+    it.todo("should fetch an array of Vpleni for a specific user and date")
+  })
+
+  describe("vnesiVplen", () => {
+    it.todo("should create a new instance of Vplen and return it if successful")
+  })
+
+  describe("izbrisiVplen", () => {
+    it.todo("should delete an instance of Vplen and return it if successful")
+  })
+})
