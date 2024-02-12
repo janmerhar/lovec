c51c7785a674ff6777f472f6518203036763baa5
Jan Merhar <merharjan@gmail.com>
2024-02-12 21:26:25 +0100

    Add SistemskeSpremenljivke.spec.ts

---
+++ server/tests/unit/SistemskeSpremenljivke.spec.ts
@@ -0,0 +1,32 @@
+import SistemskeSpremenljivke from "@entities/SistemskeSpremenljivke"
+
+import { CreateSistemskeSpremenljivkeStub } from "@stubs/CreateSistemskeSpremenljivke.stub"
+
+describe("SistemskeSpremenljivke", () => {
+  describe("constructor", () => {
+    it.todo(
+      "should create an instance of SistemskeSpremenljivke with string id and date"
+    )
+    it.todo("should create an instance of SistemskeSpremenljivke")
+  })
+
+  describe("createInstance", () => {
+    it.todo("should create an instance of SistemskeSpremenljivke")
+  })
+
+  describe("fetchSistemskeSpremenljivke", () => {
+    it.todo(
+      "should return a single instance of SistemskeSpremenljivke from the database"
+    )
+  })
+
+  describe("insertSistemskeSpremenljivke", () => {
+    it.todo("should insert a single instance of SistemskeSpremenljivke")
+  })
+
+  describe("updateSistemskeSpremenljivke", () => {
+    it.todo(
+      "should update a single instance of SistemskeSpremenljivke in the database"
+    )
+  })
+})
