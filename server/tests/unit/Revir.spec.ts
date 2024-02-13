a1b9f021b30d69df666d08d8aabc5262304eadb5
Jan Merhar <merharjan@gmail.com>
2024-02-13 18:39:42 +0100

    Add Revir.spec.ts

---
+++ server/tests/unit/Revir.spec.ts
@@ -0,0 +1,32 @@
+import Revir, { RevirDetails } from "@entities/Revir"
+import RevirModel from "@models/revirModel"
+
+describe("RevirDetails", () => {
+  describe("constructor", () => {
+    it.todo("should create an instance of RevirDetails")
+  })
+})
+
+describe("Revir", () => {
+  describe("constructor", () => {
+    it.todo("should create an instance of Revir")
+
+    it.todo("should create an instance of Revir with druzina details")
+  })
+
+  describe("fetchRevirji", () => {
+    it.todo("should return an array of Revir instances")
+  })
+
+  describe("vnesiRevir", () => {
+    it.todo("should create a new Revir instance")
+  })
+
+  describe("odstraniRevir", () => {
+    it.todo("should remove a Revir instance from the database")
+  })
+
+  describe("urediRevir", () => {
+    it.todo("should update a Revir instance in the database")
+  })
+})
