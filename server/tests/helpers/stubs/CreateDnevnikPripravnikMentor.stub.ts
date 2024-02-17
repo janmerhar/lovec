0241956ca867c14b66c20a5d76242a567fc755cf
Jan Merhar <merharjan@gmail.com>
2024-02-17 16:30:19 +0100

    Update CraeteDnevnikPripravnikMentor.stub.ts

--- server/tests/helpers/stubs/CreateDnevnikPripravnikMentor.stub.ts
+++ server/tests/helpers/stubs/CreateDnevnikPripravnikMentor.stub.ts
@@ -13,7 +13,6 @@ export const CreateDnevnikPripravnikMentorStub = (): IDnevnik<
     priimek: "1",
     slika: "1",
     role: "1",
-    isDeleted: false,
   },
   mentor: {
     _id: new mongoose.Types.ObjectId("65c78f64a7f1d997defbe16b"),
@@ -21,7 +20,6 @@ export const CreateDnevnikPripravnikMentorStub = (): IDnevnik<
     priimek: "2",
     slika: "2",
     role: "2",
-    isDeleted: false,
   },
   status: "drugo",
   datum: new Date("2024-02-10T14:23:13.888Z"),
