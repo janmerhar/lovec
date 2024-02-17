2062d6daed58e6f7fb13039bdbf77143f9273abc
Jan Merhar <merharjan@gmail.com>
2024-02-17 23:17:52 +0100

    Add new methods in Uporabnik.ts

--- server/entities/Uporabnik.ts
+++ server/entities/Uporabnik.ts
@@ -2,9 +2,11 @@ import UporabnikModel from "@models/uporabnikModel"
 import { DruzinaDetails } from "@entities/Druzina"
 import type {
   IDruzinaDetails,
+  IUporabnik,
   IUporabnikDetails,
   JWTDecoded,
   JWTPayload,
+  JWTTokenPair,
 } from "@shared/types"
 
 import type { Request } from "express"
@@ -98,12 +100,6 @@ export class UserLogin extends UporabnikDetails {
   }
 }
 
-// getUporabnik
-// - iskanje uporabnika po id/email
-
-// user profile
-// - dela ne glede na to ali je uporabnik admin / pripravnik / lovec
-
 // user login
 // - jwt
 // - refresh token
@@ -153,7 +149,10 @@ export default class Uporabnik<M = string, P = string, D = string> {
     this.refresh_token = refresh_token
   }
 
-  static async login(email: string, password: string) {
+  static async login(
+    email: string,
+    password: string
+  ): Promise<UserLogin | null> {
     const uporabnik = await UporabnikModel.findOne({ email }).exec()
 
     if (uporabnik == null) {
@@ -162,44 +161,52 @@ export default class Uporabnik<M = string, P = string, D = string> {
 
     const geslo_verify = await bcrypt.compare(password, uporabnik.hash)
 
-    if (geslo_verify) {
-      const token = this.JWTcreate({
-        uporabnikId: uporabnik._id.toString(),
-        role: uporabnik.role,
-      })
-
-      let returnUporabnik = await Uporabnik.fetchUporabnik(
-        uporabnik._id.toString()
-      )
-
-      if (returnUporabnik == null) {
-        return null
-      }
-
-      // TODO
-      // @ts-ignore
-      returnUporabnik.refresh_token = undefined
-
-      return {
-        ...returnUporabnik,
-        token,
-      }
+    if (!geslo_verify) {
+      return null
     }
 
-    return null
+    const token = this.JWTcreate({
+      uporabnikId: uporabnik._id.toString(),
+      role: uporabnik.role,
+    })
+
+    const refreshToken = this.JWTcreateRefreshToken({
+      uporabnikId: uporabnik._id.toString(),
+      role: uporabnik.role,
+    })
+
+    let returnUporabnik = await Uporabnik.insertRefreshToken(
+      uporabnik._id.toString(),
+      refreshToken
+    )
+
+    if (returnUporabnik == null) {
+      return null
+    }
+
+    return new UserLogin(
+      returnUporabnik._id.toString(),
+      returnUporabnik.ime,
+      returnUporabnik.priimek,
+      returnUporabnik.slika,
+      returnUporabnik.role,
+      token,
+      uporabnik.refresh_token
+    )
   }
 
-  //
-  // TODO: popravi, da bo delovalo za vse uporabnike
-  //
   static async register(
     ime: string,
     priimek: string,
     slika: string,
     rojstniDatum: string,
     email: string,
-    geslo: string
-  ) {
+    geslo: string,
+    role: string,
+    mentor: string | null,
+    pripravniki: string[] | null,
+    druzina: string | null
+  ): Promise<boolean> {
     const salt = await bcrypt.genSalt(10)
     const geslo_hash = await bcrypt.hash(geslo, salt)
 
@@ -210,23 +217,70 @@ export default class Uporabnik<M = string, P = string, D = string> {
       rojstniDatum,
       email,
       hash: geslo_hash,
-      role: "pripravnik",
-      mentor: null,
-      druzina: null,
+      role,
+      mentor,
+      pripravniki,
+      druzina,
     })
 
     await uporabnik.save()
 
-    // Tukaj samo vrnem true ali false
-    // glede na to ali je bila registracija uspešna
-    // Ce ni bila, se bo sprozila izjema, ki jo obravnavam v controllerju
-    // TODO: vrni registriranega uporabnika za vsak slucaj
-    return true
+    return !!uporabnik
   }
 
-  //
-  // TODO: tebe mogoce predelam v getchUserProfile
-  //
+  static async fetchUserProfile(
+    uporabnikId: string
+  ): Promise<UporabnikProfile | null> {
+    const result = await UporabnikModel.findById(uporabnikId)
+      .populate<{ mentor: IUporabnikDetails }>(
+        "mentor",
+        "_id ime priimek slika role"
+      )
+      .populate<{ pripravniki: IUporabnikDetails[] }>(
+        "pripravniki",
+        "_id ime priimek slika role"
+      )
+      .populate<{ druzina: IDruzinaDetails }>("druzina", "_id ime")
+      .exec()
+
+    if (result == null) {
+      return null
+    }
+
+    return new UporabnikProfile(
+      result._id.toString(),
+      result.ime,
+      result.priimek,
+      result.slika,
+      result.role,
+      result.mentor
+        ? new UporabnikDetails(
+            result.mentor._id.toString(),
+            result.mentor.ime,
+            result.mentor.priimek,
+            result.mentor.slika,
+            result.mentor.role
+          )
+        : null,
+      result.pripravniki
+        ? result.pripravniki?.map(
+            (pripravnik) =>
+              new UporabnikDetails(
+                pripravnik._id.toString(),
+                pripravnik.ime,
+                pripravnik.priimek,
+                pripravnik.slika,
+                pripravnik.role
+              )
+          )
+        : null,
+      result.druzina
+        ? new DruzinaDetails(result.druzina._id.toString(), result.druzina.ime)
+        : null,
+      result.isDeleted
+    )
+  }
+
   static async fetchUporabnik(
     uporabnikId: string
   ): Promise<Uporabnik<UporabnikDetails, string, DruzinaDetails> | null> {
@@ -307,6 +361,37 @@ export default class Uporabnik<M = string, P = string, D = string> {
   // JWT
   //
 
+  static async insertRefreshToken(
+    uporabnikId: string,
+    refresh_token: string
+  ): Promise<IUporabnik | null> {
+    const uporabnik = await UporabnikModel.findByIdAndUpdate(
+      uporabnikId,
+      {
+        refresh_token,
+      },
+      { new: true }
+    )
+
+    if (uporabnik == null) {
+      return null
+    }
+
+    return uporabnik
+  }
+
+  static async fetchUserByRefreshToken(
+    refreshToken: string
+  ): Promise<IUporabnik | null> {
+    const result = await UporabnikModel.findOne({ refresh_token: refreshToken })
+
+    if (result == null) {
+      return null
+    }
+
+    return result
+  }
+
   static JWTcreate(payload: JWTPayload): string {
     const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
       expiresIn: "1h",
@@ -323,6 +408,16 @@ export default class Uporabnik<M = string, P = string, D = string> {
 
       const token = req.headers.authorization.split(" ")[1]
 
+      const payload = jwt.verify(token, process.env.JWT_SECRET as string)
+
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+
+  static JWTverifyString(token: string): boolean {
+    try {
       const payload = jwt.verify(token, process.env.JWT_SECRET as string)
 
       return true
@@ -361,15 +456,41 @@ export default class Uporabnik<M = string, P = string, D = string> {
     return token
   }
 
-  //
-  // TODO:
-  //
-  static async JWTrefresh(refresh_token: string): Promise<string> {
-    /*
-     * 1. Preberi refresh token
-     * 2. Preveri ali je refresh token veljaven
-     * 3. Preveri ali je refresh token pripadal uporabniku
-     */
-    return ""
+  static async JWTrefresh(refresh_token: string): Promise<JWTTokenPair | null> {
+    const isValid = Uporabnik.JWTverifyString(refresh_token)
+
+    if (!isValid) {
+      return null
+    }
+
+    const uporabnik = await Uporabnik.fetchUserByRefreshToken(refresh_token)
+
+    if (uporabnik == null) {
+      return null
+    }
+
+    const accessToken = this.JWTcreate({
+      uporabnikId: uporabnik._id.toString(),
+      role: uporabnik.role,
+    })
+
+    const refreshToken = this.JWTcreateRefreshToken({
+      uporabnikId: uporabnik._id.toString(),
+      role: uporabnik.role,
+    })
+
+    const uporabnikWithToken = await Uporabnik.insertRefreshToken(
+      uporabnik._id.toString(),
+      refreshToken
+    )
+
+    if (uporabnikWithToken == null) {
+      return null
+    }
+
+    return {
+      accessToken,
+      refreshToken: uporabnikWithToken.refresh_token,
+    }
   }
 }
