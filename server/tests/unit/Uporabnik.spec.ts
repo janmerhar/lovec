23089db9bb57b8a6f5d1bb3f3a10546d43c05907
Jan Merhar <merharjan@gmail.com>
2024-02-17 15:47:38 +0100

    Add Uporabnik.spec.ts with constructor tests

---
+++ server/tests/unit/Uporabnik.spec.ts
@@ -0,0 +1,32 @@
+import Uporabnik, {
+  UporabnikDetails,
+  UporabnikProfile,
+} from "@entities/Uporabnik"
+
+import { DruzinaDetails } from "@entities/Druzina"
+
+describe("UporabnikDetails", () => {
+  describe("constructor", () => {
+    it.todo("should create an instance of UporabnikDetails")
+  })
+})
+
+describe("UporabnikProfile", () => {
+  describe("constructor", () => {
+    it.todo("should create an instance of UporabnikProfile for Admin")
+
+    it.todo("should create an instance of UporabnikProfile for Pripravnik")
+
+    it.todo("should create an instance of UporabnikProfile for Lovec")
+  })
+})
+
+describe("Uporabnik", () => {
+  describe("constructor", () => {
+    it.todo("should create an instance of Uporabnik for Admin")
+
+    it.todo("should create an instance of Uporabnik for Pripravnik")
+
+    it.todo("should create an instance of Uporabnik for Lovec")
+  })
+})
