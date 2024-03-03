f2dcad00d198769e1d4d49186022f4b7e6bb69d7
Jan Merhar <merharjan@gmail.com>
2024-03-03 12:39:23 +0100

    Update types.ts

--- src/client/src/types.ts
+++ src/client/src/types.ts
@@ -1,18 +1,29 @@
-export type APIResponse<T> = {
+export type APIResponse<T = any> = {
   status: number
   data: T
   message: string
 }
 
+// Tebe verjetno sploh ne rabim,
+// saj me ne briga vsebina JWT-ja,
+// to je posel serverja.
 export type JWTPayload = {
   uporabnikId: string
   role: string
 }
 
-export type UporabnikDetails = {
+export type JWTTokenPair = {
+  accessToken: string
+  refreshToken: string
+}
+
+export type UporabnikLogin = {
   id: string
   ime: string
   priimek: string
   slika: string
   role: string
+
+  token: string
+  refresh_token: string
 }
