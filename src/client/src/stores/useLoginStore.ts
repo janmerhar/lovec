7a849abd5a6de424427c1d84b61c51d30a07e7f0
Jan Merhar <merharjan@gmail.com>
2024-03-03 12:33:17 +0100

    Add persistant storage in useLoginStore.ts

--- src/client/src/stores/useLoginStore.ts
+++ src/client/src/stores/useLoginStore.ts
@@ -5,83 +5,89 @@ import { CapacitorHttp } from "@capacitor/core"
 import type { HttpResponse } from "@capacitor/core"
 import type { APIResponse, UporabnikLogin } from "@/types"
 
-export const useLoginStore = defineStore("login", () => {
-  const uporabnik = ref<UporabnikLogin | null>(null)
+export const useLoginStore = defineStore(
+  "login",
+  () => {
+    const uporabnik = ref<UporabnikLogin | null>(null)
 
-  const isLogged = computed(() => uporabnik.value !== null)
+    const isLogged = computed(() => uporabnik.value !== null)
 
-  const login = async (email: string, geslo: string): Promise<boolean> => {
-    if (isLogged.value) {
-      return true
+    const login = async (email: string, geslo: string): Promise<boolean> => {
+      if (isLogged.value) {
+        return true
+      }
+
+      const response: HttpResponse = await CapacitorHttp.post({
+        url: `${process.env.VUE_APP_API_URL}/uporabnik/login`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          email,
+          geslo,
+        },
+      })
+
+      const data: APIResponse<UporabnikLogin> = response.data
+
+      if (data.message !== "ERR_LOGIN_INVALID_CREDENTIALS") {
+        uporabnik.value = data.data
+        return true
+      }
+
+      return false
     }
 
-    const response: HttpResponse = await CapacitorHttp.post({
-      url: `${process.env.VUE_APP_API_URL}/uporabnik/login`,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        email,
-        geslo,
-      },
-    })
+    const logout = async (): Promise<boolean> => {
+      if (!isLogged.value) {
+        return true
+      }
 
-    const data: APIResponse<UporabnikLogin> = response.data
+      const response: HttpResponse = await CapacitorHttp.post({
+        url: `${process.env.VUE_APP_API_URL}/uporabnik/logout`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${uporabnik.value?.token}`,
+        },
+      })
 
-    if (data.message !== "ERR_LOGIN_INVALID_CREDENTIALS") {
-      uporabnik.value = data.data
-      return true
+      const data: APIResponse<boolean> = response.data
+
+      if (data.data) {
+        uporabnik.value = null
+        return true
+      }
+
+      return false
     }
 
-    return false
+    const token = computed(() => uporabnik.value?.token)
+    const refreshToken = computed(() => uporabnik.value?.refresh_token)
+
+    const updateToken = (token: string) => {
+      if (uporabnik.value) {
+        uporabnik.value.token = token
+      }
+    }
+
+    const updateRefreshToken = (refreshToken: string) => {
+      if (uporabnik.value) {
+        uporabnik.value.refresh_token = refreshToken
+      }
+    }
+
+    return {
+      uporabnik,
+      login,
+      logout,
+      isLogged,
+      token,
+      refreshToken,
+      updateToken,
+      updateRefreshToken,
+    }
+  },
+  {
+    persist: true,
   }
-
-  const logout = async (): Promise<boolean> => {
-    if (!isLogged.value) {
-      return true
-    }
-
-    const response: HttpResponse = await CapacitorHttp.post({
-      url: `${process.env.VUE_APP_API_URL}/uporabnik/logout`,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${uporabnik.value?.token}`,
-      },
-    })
-
-    const data: APIResponse<boolean> = response.data
-
-    if (data.data) {
-      uporabnik.value = null
-      return true
-    }
-
-    return false
-  }
-
-  const token = computed(() => uporabnik.value?.token)
-  const refreshToken = computed(() => uporabnik.value?.refresh_token)
-
-  const updateToken = (token: string) => {
-    if (uporabnik.value) {
-      uporabnik.value.token = token
-    }
-  }
-
-  const updateRefreshToken = (refreshToken: string) => {
-    if (uporabnik.value) {
-      uporabnik.value.refresh_token = refreshToken
-    }
-  }
-
-  return {
-    uporabnik,
-    login,
-    logout,
-    isLogged,
-    token,
-    refreshToken,
-    updateToken,
-    updateRefreshToken,
-  }
-})
+)
