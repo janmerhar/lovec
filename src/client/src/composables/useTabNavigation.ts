90f3d36b778f1741883388f835694205da8c96dc
Jan Merhar <merharjan@gmail.com>
2024-03-07 22:44:56 +0100

    Add useTabNavigation.ts

---
+++ src/client/src/composables/useTabNavigation.ts
@@ -0,0 +1,29 @@
+import { computed } from "vue"
+import { useIonRouter } from "@ionic/vue"
+import { useRoute } from "vue-router"
+
+export const useTabNavigation = () => {
+  const router = useIonRouter()
+  const route = useRoute()
+
+  const selectedTab = computed(() => {
+    return route.name
+  })
+
+  const selectedTabStyle = (selectedValue: string) => {
+    return {
+      "active-tab":
+        typeof selectedTab.value == "string" &&
+        selectedTab.value === selectedValue,
+    }
+  }
+
+  const redirectTo = (tabName: string) => {
+    router.push({ name: tabName })
+  }
+
+  return {
+    selectedTabStyle,
+    redirectTo,
+  }
+}
