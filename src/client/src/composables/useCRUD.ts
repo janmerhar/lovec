e3f5702c45b89cee1110b3a84d7f6fdb23d24afc
Jan Merhar <merharjan@gmail.com>
2024-03-09 14:22:20 +0100

    Add create in useCRUD.ts

--- src/client/src/composables/useCRUD.ts
+++ src/client/src/composables/useCRUD.ts
@@ -3,8 +3,26 @@ import _ from "lodash"
 
 export type DeleteFunction<T> = (data: T) => Promise<boolean>
 export const useCRUD = <T>(items: Ref<T[]>) => {
+
+  const createItem = <I>(call: CreateFunction<I, T>) => {
+    return async <T>(data: I): Promise<T | null> => {
+      // in case it fails, add to queue
+      const result = await call(data)
+
+      if (!result) {
+        return null
+      }
+
+      // in case it fails, add to queue
+      items.value.unshift(result)
+
+      return result as T
+    }
+  }
+
   const deleteItem = (call: DeleteFunction<T>) => {
     return async (original: T): Promise<boolean> => {
+      // in case it fails, add to queue
       const result = await call(original)
 
       if (!result) {
@@ -20,6 +38,7 @@ export const useCRUD = <T>(items: Ref<T[]>) => {
 
 
   return {
+    createItem,
     deleteItem,
     }
   }
