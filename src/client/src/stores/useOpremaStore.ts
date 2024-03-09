46c18d7e824a57b0e9d024c753105011f8d080a6
Jan Merhar <merharjan@gmail.com>
2024-03-09 14:37:01 +0100

    Update useOpremaStore.ts

--- src/client/src/stores/useOpremaStore.ts
+++ src/client/src/stores/useOpremaStore.ts
@@ -1,9 +1,10 @@
 import { defineStore } from "pinia"
 import { computed, ComputedRef } from "vue"
-import type { Oprema } from "@/types"
+import type { InsertOprema, Oprema } from "@/types"
 
 import { useRequest } from "@/composables/useRequest"
 import { usePagination } from "@/composables/usePagination"
+import { useCRUD } from "@/composables/useCRUD"
 
 export const useOpremaStore = defineStore(
   "oprema",
@@ -16,40 +17,39 @@ export const useOpremaStore = defineStore(
       return response.data
     }
 
-    const deleteOprema = async (opremaId: string): Promise<boolean> => {
-      const response = await request.delete<boolean>(`/oprema/${opremaId}`)
-
-      if (response) {
-        return response.data
-      }
-
-      return false
-    }
-
     const { items, fetchMore, refreshPagination } = usePagination<Oprema>(
       getOprema,
       true
     )
 
-    //   const postOprema = async (naziv: string, tip: string, stanje: string) => {}
-
-    //   const adminGetOprema = async (uporabnikId: string) => {}
-
-    //   const adminDeleteOprema = async (uporabnikId: string, opremaId: string) => {}
-
     const opremaVariable = items
     const oprema: ComputedRef<Oprema[]> = computed(() => opremaVariable.value)
 
+    const createOprema = async (oprema: InsertOprema): Promise<Oprema> => {
+      const response = await request.post<Oprema>("/oprema", {
+        url: "",
+        data: oprema,
+      })
+
+      return response.data
+    }
+
+    const deleteOprema = async (oprema: Oprema): Promise<boolean> => {
+      const response = await request.delete<boolean>(`/oprema/${oprema.id}`)
+
+      return response.data
+    }
+
+    const crud = useCRUD<Oprema>(opremaVariable)
+    const { createItem, deleteItem } = crud
+
     return {
       oprema,
-      opremaVariable,
       fetchMore,
       refreshPagination,
       getOprema,
-      deleteOprema,
-      // postOprema,
-      // adminGetOprema,
-      // adminDeleteOprema,
+      deleteItem: deleteItem(deleteOprema),
+      createItem: createItem(createOprema),
     }
   },
   {
