311ac5bf6e3cebf1a5626608611d6303a633b5e0
Jan Merhar <merharjan@gmail.com>
2024-03-10 13:20:48 +0100

    Update useObiskStore.ts

--- src/client/src/stores/useObiskStore.ts
+++ src/client/src/stores/useObiskStore.ts
@@ -1,15 +1,18 @@
 import { defineStore } from "pinia"
 import { computed, ComputedRef, ref } from "vue"
 
-import type { Obisk, Opazovalnica } from "@/types"
+import type { Obisk, Opazovalnica, InsertObisk } from "@/types"
 import { useRequest } from "@/composables/useRequest"
 import { usePagination } from "@/composables/usePagination"
+import { useCRUD } from "@/composables/useCRUD"
 
 export const useObiskStore = defineStore("obisk", () => {
   const { request } = useRequest()
 
   const selectedOpazovalnica = ref<Opazovalnica | null>(null)
   const selectedDatum = ref<string>(new Date().toISOString().split("T")[0])
+
+  const activeObisk = ref<Obisk | null>(null)
 
   const fetchObiski = async (
     chosenOpazovalnica: Opazovalnica,
@@ -33,6 +36,14 @@ export const useObiskStore = defineStore("obisk", () => {
     }
 
     return []
+  }
+
+  const fetchActiveObisk = async (): Promise<Obisk | null> => {
+    const response = await request.get<Obisk | null>("/obiski/aktivni")
+
+    activeObisk.value = response.data
+
+    return response.data
   }
 
   const setDatum = async (newDatum?: string) => {
@@ -63,6 +74,34 @@ export const useObiskStore = defineStore("obisk", () => {
   const obiski: ComputedRef<Obisk[]> = computed(() => obiskiVariable.value)
   const opazovalnica = computed(() => selectedOpazovalnica.value)
 
+  //
+  // CRUD operations
+  //
+
+  const createObisk = async (obisk: InsertObisk): Promise<Obisk> => {
+    const response = await request.post<Obisk>("/obiski", {
+      url: "",
+      data: obisk,
+    })
+
+    activeObisk.value = response.data
+
+    return response.data
+  }
+
+  const endObisk = async (): Promise<Obisk> => {
+    const response = await request.patch<Obisk>(`/obiski/aktivni`)
+
+    if (response.data !== null) {
+      activeObisk.value = null
+    }
+
+    return response.data
+  }
+
+  const crud = useCRUD<Obisk>(obiskiVariable)
+  const { createItem, updateItem } = crud
+
   return {
     opazovalnica,
     obiski,
@@ -70,5 +109,9 @@ export const useObiskStore = defineStore("obisk", () => {
     setDatum,
     fetchMore,
     refreshPagination,
+    activeObisk,
+    fetchActiveObisk,
+    createItem: createItem(createObisk),
+    updateItem: updateItem(endObisk),
   }
 })
