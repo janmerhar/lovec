f888821cffe356214f4bbedb1f17dcc376286757
Jan Merhar <merharjan@gmail.com>
2024-03-15 22:30:58 +0100

    Update ButtonRow.vue

--- src/client/src/components/ui-components/button/ButtonRow.vue
+++ src/client/src/components/ui-components/button/ButtonRow.vue
@@ -1,5 +1,12 @@
 <template>
-  <div style="display: flex; justify-content: space-around; width: 85%">
+  <div
+    style="
+      display: flex;
+      justify-content: space-around;
+      width: 85%;
+      margin-top: 7px;
+    "
+  >
     <slot></slot>
   </div>
 </template>
