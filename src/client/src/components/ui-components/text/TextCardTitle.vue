958789baac4b3b8c1fb9de47258922916624de7b
Jan Merhar <merharjan@gmail.com>
2024-03-15 17:29:38 +0100

    Add TextCardTitle.vue

---
+++ src/client/src/components/ui-components/text/TextCardTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3
+    style="
+      font-size: 1.3rem;
+      text-transform: uppercase;
+      padding-bottom: 0rem;
+      margin-bottom: 0rem;
+      font-weight: 800;
+      letter-spacing: 2px;
+    "
+  >
+    <slot></slot>
+  </h3>
+</template>
