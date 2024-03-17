7e6f1ab003d47cc48c2527904259b2975ac47961
Jan Merhar <merharjan@gmail.com>
2024-03-17 01:37:30 +0100

    Add CardNormal.vue

---
+++ src/client/src/components/ui-components/card/CardNormal.vue
@@ -0,0 +1,15 @@
+<template>
+  <ion-card
+    style="
+      border-radius: 20px;
+      box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+      padding: 1rem;
+    "
+  >
+    <slot></slot>
+  </ion-card>
+</template>
+
+<script setup lang="ts">
+import { IonCard } from "@ionic/vue"
+</script>
