9f76dab0503ca6aa4fbbc9cfc13376bf89e8c0e3
Jan Merhar <merharjan@gmail.com>
2024-03-17 01:39:28 +0100

    Update TabJage.vue

--- src/client/src/views/TabJage.vue
+++ src/client/src/views/TabJage.vue
@@ -1,26 +1,23 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      <refresher-component :refresh="refresh"></refresher-component>
+      <ion-button @click="selectItem(1)">select 1</ion-button>
+      <ion-button @click="selectItem(2)">select 2</ion-button>
+      <ion-button @click="selectItem(3)">select 3</ion-button>
 
-      <card-jaga @join="openModalJagaDescription">banana</card-jaga>
-      <card-jaga @join="openModalJagaDescription">ananab</card-jaga>
+      <refresher-component :refresh="refreshPagination"></refresher-component>
 
-      <card-jaga
-        @card="openModalJagaDescription"
-        @join="openModalJagaDescription"
-        >eggplant</card-jaga
-      >
-      <card-jaga @join="openModalJagaDescription">peach</card-jaga>
-      <card-jaga @join="openModalJagaDescription">peachy</card-jaga>
+      <template v-for="jaga in jagaStore.jage" :key="jaga.id">
+        <card-jaga :jaga="jaga">banana</card-jaga>
+      </template>
 
-      <fab-button @click.prevent="openModalJagaAdd"></fab-button>
+      <fab-button @click.prevent="() => {}"></fab-button>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonPage, IonContent, modalController } from "@ionic/vue"
+<script setup lang="ts">
+import { IonPage, IonContent, modalController, IonButton } from "@ionic/vue"
 
 import FabButton from "@/components/FabButtonAdd.vue"
 import CardJaga from "@/components/CardJaga.vue"
@@ -28,54 +25,46 @@ import ModalJagaAdd from "@/components/ModalJagaAdd.vue"
 import ModalJagaDescription from "@/components/ModalJagaDescription.vue"
 import RefresherComponent from "@/components/ui-components/RefresherComponent.vue"
 
-import { defineComponent } from "vue"
+import { useJagaStore } from "@/stores/useJagaStore"
+import { onBeforeMount } from "vue"
 
-export default defineComponent({
-  components: {
-    IonPage,
-    IonContent,
-    FabButton,
-    CardJaga,
-    RefresherComponent,
-  },
-  setup() {
-    return { message: "", isOpen: false }
-  },
-  methods: {
-    async openModalJagaAdd() {
-      console.warn("click ModalJagaAdd")
-      const modal = await modalController.create({
-        component: ModalJagaAdd,
-      })
-      modal.present()
+const jagaStore = useJagaStore()
+const { refreshPagination, selectItem } = jagaStore
 
-      const { data, role } = await modal.onWillDismiss()
-
-      if (role === "confirm") {
-        this.message = `Hello, ${data}!`
-      }
-    },
-
-    async openModalJagaDescription() {
-      console.warn("click ModalJagaDescription ")
-      const modal = await modalController.create({
-        component: ModalJagaDescription,
-      })
-      modal.dismiss()
-      modal.present()
-
-      const { data, role } = await modal.onWillDismiss()
-
-      if (role === "confirm") {
-        this.message = `Hello, ${data}!`
-      }
-    },
-
-    async refresh(event: CustomEvent) {
-      setTimeout(() => {
-        event.detail.complete()
-      }, 2000)
-    },
-  },
+onBeforeMount(async () => {
+  await refreshPagination()
 })
+
+// export default defineComponent({
+//   methods: {
+//     async openModalJagaAdd() {
+//       console.warn("click ModalJagaAdd")
+//       const modal = await modalController.create({
+//         component: ModalJagaAdd,
+//       })
+//       modal.present()
+
+//       const { data, role } = await modal.onWillDismiss()
+
+//       if (role === "confirm") {
+//         this.message = `Hello, ${data}!`
+//       }
+//     },
+
+//     async openModalJagaDescription() {
+//       console.warn("click ModalJagaDescription ")
+//       const modal = await modalController.create({
+//         component: ModalJagaDescription,
+//       })
+//       modal.dismiss()
+//       modal.present()
+
+//       const { data, role } = await modal.onWillDismiss()
+
+//       if (role === "confirm") {
+//         this.message = `Hello, ${data}!`
+//       }
+//     },
+//   },
+// })
 </script>
