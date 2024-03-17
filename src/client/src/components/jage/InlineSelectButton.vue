47e0c2baed4e548265d1069c66dd2d3f95006892
Jan Merhar <merharjan@gmail.com>
2024-03-17 14:48:53 +0100

    Add InlineSelectButton.vue

---
+++ src/client/src/components/jage/InlineSelectButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <ion-button :color="color"> <slot> </slot> </ion-button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  color: {
+    type: String,
+    default: "primary",
+  },
+})
+</script>
+
+<style scoped>
+ion-button {
+  --border-radius: 1000000px !important;
+}
+</style>
