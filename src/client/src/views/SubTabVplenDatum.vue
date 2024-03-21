115b9177a3e360fcfb9a312540ac2a338960899e
Jan Merhar <merharjan@gmail.com>
2024-03-21 22:41:39 +0100

    Add SubTabVplenDatum.vue

---
+++ src/client/src/views/SubTabVplenDatum.vue
@@ -0,0 +1,61 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <refresher-component
+        :refresh="vplenStore.refreshPagination"
+      ></refresher-component>
+
+      <h3 class="ion-text-center">Vpleni za dam ena dva tri</h3>
+      <!-- Vplen cards -->
+
+      <template v-for="vplen in vplenStore.vpleni" :key="vplen.id">
+        <card-vplen-description :vplen="vplen"></card-vplen-description>
+      </template>
+
+      <fab-button-add
+        @click.prevent="openModal(ModalVplenAdd)"
+      ></fab-button-add>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonContent } from "@ionic/vue"
+import { onBeforeMount } from "vue"
+
+import FabButtonAdd from "@/components/FabButtonAdd.vue"
+import CardVplenDescription from "@/components/vplen/CardVplenDescription.vue"
+
+import ModalVplenAdd from "@/components/vplen/ModalVplenAdd.vue"
+import RefresherComponent from "@/components/ui-components/RefresherComponent.vue"
+
+import { useVplenStore } from "@/stores/useVplenStore"
+import { useRoute } from "vue-router"
+import { useVplenDetailsStore } from "@/stores/useVplenDetailsStore"
+import { useModal } from "@/composables/useModal"
+import { useTabNavigation } from "@/composables/useTabNavigation"
+import { useDate } from "@/composables/useDate"
+const { openModal } = useModal()
+
+const vplenDetailsStore = useVplenDetailsStore()
+const vplenStore = useVplenStore()
+
+const { setVplenDetails } = vplenStore
+
+const route = useRoute()
+
+onBeforeMount(async () => {
+  const id = route.params.id
+
+  const selectedVplen = vplenDetailsStore.vpleniDetails.filter(
+    (vplen) => vplen.datum === id
+  )[0]
+
+  if (!selectedVplen) {
+    useTabNavigation().redirectTo("vpleni")
+    return
+  }
+
+  await setVplenDetails(selectedVplen)
+})
+</script>
