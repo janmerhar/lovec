782d2585330e8b7c6ba9bc8883957c52960dbfc4
Jan Merhar <merharjan@gmail.com>
2024-03-24 00:34:50 +0100

    Update ModalJagaAdd.vue

--- src/client/src/components/ModalJagaAdd.vue
+++ src/client/src/components/ModalJagaAdd.vue
@@ -1,49 +1,26 @@
 <template>
-  <header-modal @cancel="cancel()" @confirm="confirm()">
-    Dodaj Jago
-  </header-modal>
-  <ion-content class="ion-padding">
-    <ion-item>
-      <ion-label position="stacked">Your name</ion-label>
-      <ion-input v-model="name" placeholder="Your name"></ion-input>
-    </ion-item>
-  </ion-content>
+  <modal-template>
+    <template #header>
+      <modal-header
+        @confirm="useModal().confirmModal"
+        @cancel="useModal().cancelModal"
+        >abc</modal-header
+      >
+    </template>
+
+    <template #body>
+      <ion-item>
+        <ion-label position="stacked">Your name</ion-label>
+        <ion-input placeholder="Your name"></ion-input>
+      </ion-item>
+    </template>
+  </modal-template>
 </template>
 
-<script lang="ts">
-import {
-  IonContent,
-  IonItem,
-  IonLabel,
-  IonInput,
-  modalController,
-} from "@ionic/vue"
+<script setup lang="ts">
+import { IonItem, IonLabel, IonInput } from "@ionic/vue"
 
-import { defineComponent } from "vue"
-
-import HeaderModal from "./ui-components/HeaderModal.vue"
-
-export default defineComponent({
-  name: "ModalJagaAdd",
-  components: {
-    IonContent,
-    IonItem,
-    IonLabel,
-    IonInput,
-    HeaderModal,
-  },
-  data() {
-    return {
-      name: "",
-    }
-  },
-  methods: {
-    cancel() {
-      return modalController.dismiss(null, "cancel")
-    },
-    confirm() {
-      return modalController.dismiss(this.name, "confirm")
-    },
-  },
-})
+import ModalTemplate from "@/components/ui-components/modal/ModalTemplate.vue"
+import ModalHeader from "@/components/ui-components/modal/ModalHeader.vue"
+import { useModal } from "@/composables/useModal"
 </script>
