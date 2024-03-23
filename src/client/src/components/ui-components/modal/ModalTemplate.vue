8ce46ad93ae3975fb6a7d49915c0f4a1579a3b55
Jan Merhar <merharjan@gmail.com>
2024-03-23 13:45:51 +0100

    Update ModalTemplate.vue

--- src/client/src/components/ui-components/modal/ModalTemplate.vue
+++ src/client/src/components/ui-components/modal/ModalTemplate.vue
@@ -1,6 +1,18 @@
 <template>
   <slot name="header"></slot>
-  <ion-content class="ion-padding"> <slot name="body"></slot> </ion-content>
+  <ion-content color="primary">
+    <div class="ion-padding modal-body">
+      <slot name="body"></slot>
+    </div>
+  </ion-content>
 </template>
 
-<style scoped></style>
+<style scoped>
+.modal-body {
+  width: 100%;
+  height: 100%;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  background-color: var(--ion-background-color);
+}
+</style>
