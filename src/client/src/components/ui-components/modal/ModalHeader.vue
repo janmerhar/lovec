723aa974f617cefc67992cfba3e37543be029492
Jan Merhar <merharjan@gmail.com>
2024-03-23 13:26:00 +0100

    Update ModalHeader.vue

--- src/client/src/components/ui-components/modal/ModalHeader.vue
+++ src/client/src/components/ui-components/modal/ModalHeader.vue
@@ -1,34 +1,26 @@
 <template>
-  <ion-header>
-    <ion-toolbar>
-      <ion-buttons slot="start">
-        <ion-button @click.prevent="emit('cancel')">
-          <font-awesome-icon :icon="['fas', 'arrow-left']" size="xl" />
-        </ion-button>
-      </ion-buttons>
+  <ion-toolbar color="primary">
+    <ion-buttons slot="start">
+      <ion-button @click.prevent="emit('cancel')">
+        <font-awesome-icon :icon="['fas', 'arrow-left']" size="xl" />
+      </ion-button>
+    </ion-buttons>
 
-      <ion-title class="ion-text-center">
-        <!-- slot for providing title -->
-        <slot></slot>
-      </ion-title>
+    <ion-title slot="" class="ion-text-center header-text">
+      <!-- slot for providing title -->
+      <slot></slot>
+    </ion-title>
 
-      <ion-buttons slot="end" v-if="confirmButton">
-        <ion-button @click.prevent="emit('confirm')">
-          <font-awesome-icon :icon="['fas', 'plus']" size="xl" />
-        </ion-button>
-      </ion-buttons>
-    </ion-toolbar>
-  </ion-header>
+    <ion-buttons slot="end" style="min-width: 38px">
+      <ion-button @click.prevent="emit('confirm')" v-if="confirmButton">
+        <font-awesome-icon :icon="['fas', 'check']" size="xl" />
+      </ion-button>
+    </ion-buttons>
+  </ion-toolbar>
 </template>
 
 <script setup lang="ts">
-import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonButtons,
-  IonButton,
-} from "@ionic/vue"
+import { IonToolbar, IonTitle, IonButtons, IonButton } from "@ionic/vue"
 
 const emit = defineEmits(["confirm", "cancel"])
 
@@ -40,3 +32,12 @@ defineProps({
   },
 })
 </script>
+
+<style scoped>
+.header-text {
+  text-transform: uppercase;
+  font-size: 20px;
+  letter-spacing: 1px;
+  font-weight: 500;
+}
+</style>
