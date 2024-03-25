c5cf7c72d397425f9266f18deefe1332fc1edb6d
Jan Merhar <merharjan@gmail.com>
2024-03-25 19:30:02 +0100

    Update TabVplen.vue

--- src/client/src/views/TabVplen.vue
+++ src/client/src/views/TabVplen.vue
@@ -1,11 +1,9 @@
 <template>
-  <ion-page>
-    <ion-content>
-      <refresher-component :refresh="refresh"></refresher-component>
-
-      <h3 class="ion-text-center">Zgodovina vplenov</h3>
-      <!-- Vplen cards -->
-
+  <tab-template :refresh="refreshPagination" :scroll="fetchMore">
+    <template #header>
+      <tab-header>Zgodovina vplenov</tab-header>
+    </template>
+    <template #body>
       <template
         v-for="vplen in vplenDetailsStore.vpleniDetails"
         :key="new Date(vplen.datum).getTime()"
@@ -19,34 +17,24 @@
       <fab-button-add
         @click.prevent="openModal(ModalVplenAdd)"
       ></fab-button-add>
-
-      <infinite-scroll-component
-        :scroll="fetchMore"
-      ></infinite-scroll-component>
-    </ion-content>
-  </ion-page>
+    </template>
+  </tab-template>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonContent, modalController } from "@ionic/vue"
 import { onBeforeMount } from "vue"
 
 import FabButtonAdd from "@/components/FabButtonAdd.vue"
 import CardVplenDetails from "@/components/vplen/CardVplenDetails.vue"
 
 import ModalVplenAdd from "@/components/vplen/ModalVplenAdd.vue"
-import RefresherComponent from "@/components/ui-components/RefresherComponent.vue"
-import InfiniteScrollComponent from "@/components/ui-components/InfiniteScrollComponent.vue"
+import TabTemplate from "@/components/ui-components/tab/TabTemplate.vue"
+import TabHeader from "@/components/ui-components/tab/TabHeader.vue"
 
 import { useVplenDetailsStore } from "@/stores/useVplenDetailsStore"
 
 const vplenDetailsStore = useVplenDetailsStore()
 const { fetchMore, refreshPagination } = vplenDetailsStore
-
-const refresh = async (event: CustomEvent) => {
-  await refreshPagination()
-  event.detail.complete()
-}
 
 onBeforeMount(async () => {
   await fetchMore()
