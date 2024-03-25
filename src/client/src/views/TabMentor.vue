6f09ab567550aab02f6007effd88ce18e2500a85
Jan Merhar <merharjan@gmail.com>
2024-03-25 19:37:41 +0100

    Update TabMentor.vue

--- src/client/src/views/TabMentor.vue
+++ src/client/src/views/TabMentor.vue
@@ -1,14 +1,17 @@
 <template>
-  <ion-page>
-    <ion-content>
-      <refresher-component :refresh="refreshPagination"></refresher-component>
-
+  <tab-template :refresh="refreshPagination" :scroll="fetchMore">
+    <template #header>
+      <tab-header>Dnevniki</tab-header>
+    </template>
+    <template #body>
       <ion-button @click="updateItemPotrjen(mentorStore.dnevniki[0])"
         >update dnevnik</ion-button
       >
       <ion-button @click="selectItem('2019-01-02')">change datum</ion-button>
 
-      <h3 class="ion-text-center">Dnevniki</h3>
+      <datepicker-horizontal
+        @change="(datum) => selectItem(datum)"
+      ></datepicker-horizontal>
 
       <template v-for="dnevnik in mentorStore.dnevniki" :key="dnevnik.id">
         <card-dnevnik-description
@@ -18,27 +21,17 @@
           @reject="updateItemZavrnjen(dnevnik)"
         ></card-dnevnik-description>
       </template>
-
-      <infinite-scroll-component
-        :scroll="fetchMore"
-        v-if="true"
-      ></infinite-scroll-component>
-    </ion-content>
-  </ion-page>
+    </template>
+  </tab-template>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonContent } from "@ionic/vue"
-
 import { onBeforeMount } from "vue"
 
+import DatepickerHorizontal from "@/components/ui-components/DatepickerHorizontal.vue"
 import CardDnevnikDescription from "@/components/pripravniki/CardDnevnikDescription.vue"
-import RefresherComponent from "@/components/ui-components/RefresherComponent.vue"
-import InfiniteScrollComponent from "@/components/ui-components/InfiniteScrollComponent.vue"
-
-//
-// Nova koda
-//
+import TabTemplate from "@/components/ui-components/tab/TabTemplate.vue"
+import TabHeader from "@/components/ui-components/tab/TabHeader.vue"
 
 import { useMentorDnevnikStore } from "@/stores/useMentorDnevnikStore"
 
@@ -54,16 +47,4 @@ const {
 onBeforeMount(async () => {
   await fetchMore()
 })
-
-// export default defineComponent({
-//   methods: {
-//     async openModalPripravnikAdd() {
-//       const modal = await modalController.create({
-//         component: ModalDnevnikAdd,
-//         componentProps: {
-//           mentorIme: this.uporabnikStore.mentorIme,
-//         },
-//       })
-//       modal.present()
-//     },
 </script>
