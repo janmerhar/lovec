528af2e2415270f57f4be7d7476541ba089b4553
Jan Merhar <merharjan@gmail.com>
2024-03-25 19:11:33 +0100

    Update TabTemplate.vue with scroll and refresh

--- src/client/src/components/ui-components/tab/TabTemplate.vue
+++ src/client/src/components/ui-components/tab/TabTemplate.vue
@@ -3,7 +3,16 @@
     <slot name="header"></slot>
     <ion-content color="primary fade-in">
       <ion-content id="tab-template-body" class="brd">
+        <!--  -->
+        <refresher-component :refresh="refresh"></refresher-component>
+
         <slot name="body"></slot>
+
+        <infinite-scroll-component
+          :scroll="scroll"
+          v-if="true"
+        ></infinite-scroll-component>
+        <!--  -->
       </ion-content>
     </ion-content>
   </ion-page>
@@ -11,6 +20,24 @@
 
 <script setup lang="ts">
 import { IonPage, IonContent } from "@ionic/vue"
+
+import RefresherComponent from "@/components/ui-components/RefresherComponent.vue"
+import InfiniteScrollComponent from "@/components/ui-components/InfiniteScrollComponent.vue"
+
+defineProps({
+  refresh: {
+    type: Function,
+    default: (): void => {
+      // do nothing
+    },
+  },
+  scroll: {
+    type: Function,
+    default: () => {
+      // do nothing
+    },
+  },
+})
 </script>
 
 <style scoped>
