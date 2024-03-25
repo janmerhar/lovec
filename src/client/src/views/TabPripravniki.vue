39cce0f09c1a28a8051f6ca7c4aabbf2e7c78970
Jan Merhar <merharjan@gmail.com>
2024-03-25 19:11:43 +0100

    Update TabPripravniki.vue

--- src/client/src/views/TabPripravniki.vue
+++ src/client/src/views/TabPripravniki.vue
@@ -1,9 +1,9 @@
 <template>
-  <ion-page>
-    <ion-content :fullscreen="true">
-      <refresher-component :refresh="refreshPagination"></refresher-component>
-
-      <h3 class="ion-text-center">Dnevniki</h3>
+  <tab-template :refresh="refreshPagination" :scroll="fetchMore">
+    <template #header>
+      <tab-header>Dnevniki</tab-header>
+    </template>
+    <template #body>
       <template v-for="dnevnik in pripravnikStore.dnevniki" :key="dnevnik.id">
         <card-dnevnik-description :dnevnik="dnevnik"></card-dnevnik-description>
       </template>
@@ -11,25 +11,18 @@
       <fab-button-add
         @click.prevent="openModal(ModalDnevnikAdd)"
       ></fab-button-add>
-
-      <infinite-scroll-component
-        :scroll="fetchMore"
-        v-if="true"
-      ></infinite-scroll-component>
-    </ion-content>
-  </ion-page>
+    </template>
+  </tab-template>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonContent } from "@ionic/vue"
-
 import { onBeforeMount } from "vue"
 
 import FabButtonAdd from "@/components/FabButtonAdd.vue"
 import ModalDnevnikAdd from "@/components/pripravniki/ModalDnevnikAdd.vue"
 import CardDnevnikDescription from "@/components/pripravniki/CardDnevnikDescription.vue"
-import RefresherComponent from "@/components/ui-components/RefresherComponent.vue"
-import InfiniteScrollComponent from "@/components/ui-components/InfiniteScrollComponent.vue"
+import TabTemplate from "@/components/ui-components/tab/TabTemplate.vue"
+import TabHeader from "@/components/ui-components/tab/TabHeader.vue"
 
 import { usePripravnikDnevnikStore } from "@/stores/usePripravnikDnevnikStore"
 import { useModal } from "@/composables/useModal"
