c45731c097af46a511e44c218cd6df9e547ac041
Jan Merhar <merharjan@gmail.com>
2024-03-25 18:52:40 +0100

    Add TabHeader.vue

---
+++ src/client/src/components/ui-components/tab/TabHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3 class="ion-text-center">
+    <slot></slot>
+  </h3>
+</template>
+
+<style scoped>
+h3 {
+  text-transform: uppercase;
+  font-size: 20px;
+  letter-spacing: 1px;
+  font-weight: 500;
+  margin: 0;
+  padding: 5px;
+  background-color: var(--ion-color-primary);
+  color: var(--ion-color-light);
+}
+</style>
