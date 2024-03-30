fd731ee272447dd6319b6437f44e43f87c8cdf3e
Jan Merhar <merharjan@gmail.com>
2024-03-30 12:59:37 +0100

    Update ModalDnevnikAdd.vue

--- src/client/src/components/pripravniki/ModalDnevnikAdd.vue
+++ src/client/src/components/pripravniki/ModalDnevnikAdd.vue
@@ -7,57 +7,43 @@
         >Vnesi dnevnik</modal-header
       >
     </template>
+    <datepicker-horizontal class="ion-margin-bottom"></datepicker-horizontal>
     <template #body>
-      <datepicker-horizontal class="ion-margin-bottom"></datepicker-horizontal>
-      <!--
-      Izbira mentorja
-      - mentor je ze izbran, zato uporabnik nima izbire
-      -->
-      <ion-item fill="solid" class="ion-margin-vertical">
-        <ion-label position="stacked">Mentor</ion-label>
-        <ion-input type="text" :value="'abc'" disabled></ion-input>
-      </ion-item>
-      <!--
-      Nastavi input na dropdown
-      -->
-      <ion-item fill="solid" class="ion-margin-bottom">
-        <ion-label position="stacked">Delo</ion-label>
-        <ion-input
-          placeholder="Delo"
+      <Form
+        :validation-schema="dnevnikAddSchema"
+        @submit="usePripravnikDnevnikStore().createItem(insertDnevnik)"
+      >
+        <Field as="select" name="delo" v-model="insertDnevnik.delo">
+          <option v-for="delo in deloDomain" :key="delo" :value="delo">
+            {{ delo }}
+          </option>
+        </Field>
+        <ErrorMessage name="delo" />
+        <br />
+
+        <Field name="ure" type="number" v-model="insertDnevnik.ure" />
+        <ErrorMessage name="ure" />
+        <br />
+
+        <Field
+          as="textarea"
+          name="opis"
           type="text"
-          v-model="delo"
-          required
-        ></ion-input>
-      </ion-item>
-      <!--  -->
-      <ion-item fill="solid" class="ion-margin-top">
-        <ion-label position="stacked">Ure</ion-label>
-        <ion-input
-          placeholder="Ure"
-          type="number"
-          v-model="ure"
-          required
-        ></ion-input>
-      </ion-item>
-      <!--  -->
-      <ion-item fill="solid" class="ion-margin-top">
-        <ion-label position="stacked">Opis</ion-label>
-        <ion-textarea
-          label="Solid textarea"
-          labelPlacement="floating"
-          :auto-grow="true"
-          :rows="6"
-          fill="solid"
-          placeholder="Vnesi opis"
-          v-model="opis"
-        ></ion-textarea>
-      </ion-item>
+          rows="5"
+          v-model="insertDnevnik.opis"
+        />
+        <ErrorMessage name="opis" />
+        <br />
+
+        <button type="submit">
+          {{ $t("pripravniki.crud.create.button") }}
+        </button>
+      </Form>
     </template>
   </modal-template>
 </template>
 
 <script setup lang="ts">
-import { IonItem, IonLabel, IonInput, IonTextarea } from "@ionic/vue"
 import { ref } from "vue"
 
 import DatepickerHorizontal from "@/components/ui-components/DatepickerHorizontal.vue"
@@ -65,10 +51,17 @@ import DatepickerHorizontal from "@/components/ui-components/DatepickerHorizonta
 import ModalTemplate from "@/components/ui-components/modal/ModalTemplate.vue"
 import ModalHeader from "@/components/ui-components/modal/ModalHeader.vue"
 import { useModal } from "@/composables/useModal"
+import { Form, Field, ErrorMessage } from "vee-validate"
 
-const datum = ref(new Date().toISOString().slice(0, 10))
-const delo = ref("")
-const ure = ref(1)
-const opis = ref("")
-const name = ref("")
+import { dnevnikAddSchema } from "@/text-validation/dnevnikSchemas"
+import type { InsertDnevnik } from "@/types"
+import { deloDomain } from "@/types"
+import { usePripravnikDnevnikStore } from "@/stores/usePripravnikDnevnikStore"
+
+const insertDnevnik = ref<InsertDnevnik>({
+  datum: "2024-03-29",
+  delo: "",
+  ure: 0,
+  opis: "",
+})
 </script>
