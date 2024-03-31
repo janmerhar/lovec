559272ea366d18f7282647862296e5076c04163b
Jan Merhar <merharjan@gmail.com>
2024-03-31 19:56:46 +0200

    Add vplenSchemas.ts

---
+++ src/client/src/text-validation/vplenSchemas.ts
@@ -0,0 +1,11 @@
+import * as yup from "yup"
+
+import { zivalDomain, bolezenDomain } from "@/types"
+
+export const vplenAddSchema = yup.object({
+  koordinate: yup.array().of(yup.number()).length(2) /*.required()*/,
+  zival: yup.string().oneOf(zivalDomain).required(),
+  teza: yup.number().min(0).required(),
+  datum: yup.date() /*.required()*/,
+  bolezni: yup.array().of(yup.string().oneOf(bolezenDomain)).required(),
+})
