fc58373b0e71dc0a9bb7dfd517306ff00071ef2d
Jan Merhar <merharjan@gmail.com>
2024-04-01 22:29:12 +0200

    Update TabLogin.vue

--- src/client/src/views/TabLogin.vue
+++ src/client/src/views/TabLogin.vue
@@ -1,24 +1,77 @@
 <template>
-  <tab-template>
-    <template #header>
-      <img src="" class="center" />
-    </template>
-    <template #body>
-      <Form :validation-schema="loginSchema" @submit="loginRedirect">
-        <Field name="email" type="email" v-model="inputData.email" />
-        <ErrorMessage name="email" />
-        <br />
-        <Field name="password" type="password" v-model="inputData.password" />
-        <ErrorMessage name="password" />
+  <div
+    style="
+      height: 100vh;
+      box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.15);
+      background-image: url('https://images.unsplash.com/photo-1503435980610-a51f3ddfee50');
+      background-size: 200%;
+      background-position: 60% 60%;
+      z-index: -1;
+    "
+  >
+    <Form
+      :validation-schema="loginSchema"
+      @submit="loginRedirect"
+      style="
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        gap: 5px;
+      "
+    >
+      <div style="position: fixed; top: 15px; right: 20px">ICON</div>
+      <!--  -->
+      <input-label-left>
+        <template #icon
+          ><font-awesome-icon
+            style="padding-left: 0.5rem; padding-top: 1rem"
+            :icon="['fas', 'people-group']"
+            fixed-width
+        /></template>
+        <template #input>
+          <Field
+            name="email"
+            type="email"
+            v-model="inputData.email"
+            style="
+              background-color: var(--ion-color-step-300);
+              border: none;
+              padding: 0.5rem;
+              margin: 0;
+            "
+          />
+        </template>
+        <template #error>
+          <ErrorMessage name="email" />
+        </template>
+      </input-label-left>
 
-        <button type="submit">{{ $t("login.tab.input.submit") }}</button>
-      </Form>
-    </template>
-  </tab-template>
+      <Field name="password" type="password" v-model="inputData.password" />
+      <ErrorMessage name="password" />
+
+      <button
+        type="submit"
+        style="
+          position: fixed;
+          bottom: 50px;
+          width: 80%;
+          height: 2.5rem;
+          text-transform: uppercase;
+          font-weight: 700;
+          letter-spacing: 1px;
+          border-radius: 10px;
+        "
+      >
+        {{ $t("login.tab.input.submit") }}
+      </button>
+    </Form>
+  </div>
 </template>
 
 <script setup lang="ts">
-import TabTemplate from "@/components/ui-components/tab/TabTemplate.vue"
+import InputLabelLeft from "@/components/ui-components/input/InputLabelLeft.vue"
 
 import { useLoginStore } from "@/stores/useLoginStore"
 import { useTabNavigation } from "@/composables/useTabNavigation"
@@ -29,6 +82,7 @@ const t = i18n.global.t
 
 import { Form, Field, ErrorMessage } from "vee-validate"
 import { loginSchema } from "@/text-validation/loginSchemas"
+import { storeToRefs } from "pinia"
 
 // Initial values
 const inputData = {
@@ -39,11 +93,17 @@ const inputData = {
 const { login } = useLoginStore()
 const { redirectTo } = useTabNavigation()
 
+const { isAdmin } = storeToRefs(useLoginStore())
+
 const loginRedirect = async () => {
   const result = await login(inputData.email, inputData.password)
 
   if (result) {
-    redirectTo("oprema")
+    if (isAdmin.value) {
+      redirectTo("admin_druzine")
+    } else {
+      redirectTo("oprema")
+    }
   } else {
     useAlert().errorToast(t("login.tab.alert.loginError"))
   }
@@ -58,8 +118,8 @@ const loginRedirect = async () => {
 }
 
 img {
-  height: 50vh;
-  width: 100%;
+  height: 100vh;
+  background-size: cover;
 }
 
 .button {
