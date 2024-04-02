3c9fb914e994f2645ad9c2fc182c706effcc6b78
Jan Merhar <merharjan@gmail.com>
2024-04-02 20:18:06 +0200

    Update TabAdminDruzine.vue

--- src/client/src/views/admin/TabAdminDruzine.vue
+++ src/client/src/views/admin/TabAdminDruzine.vue
@@ -1,9 +1,44 @@
-<template><p>tab admin druzine</p></template>
+<template>
+  <tab-template :refresh="refreshPagination" :scroll="fetchMore">
+    <template #header>
+      <tab-header>{{ $t("admin_druzine.tab.header") }}</tab-header>
+    </template>
+    <template #body>
+      <TransitionGroup name="list" tag="ul">
+        <li v-for="elDruzina in druzine" :key="elDruzina.id">
+          <card-druzina-details :druzina="elDruzina"></card-druzina-details>
+        </li>
+      </TransitionGroup>
+
+      <tab-no-elements :elements="druzine"></tab-no-elements>
+
+      <fab-button-add
+        @click.prevent="openModal(ModalDruzinaAdd)"
+      ></fab-button-add>
+    </template>
+  </tab-template>
+</template>
 
 <script setup lang="ts">
-import { onMounted } from "vue"
+import { onBeforeMount } from "vue"
 
-onMounted(() => {
-  console.log("TabAdminDruzine.vue mounted")
+import FabButtonAdd from "@/components/FabButtonAdd.vue"
+import CardDruzinaDetails from "@/components/admin/druzina/CardDruzinaDetails.vue"
+import TabTemplate from "@/components/ui-components/tab/TabTemplate.vue"
+import TabHeader from "@/components/ui-components/tab/TabHeader.vue"
+import TabNoElements from "@/components/ui-components/tab/TabNoElements.vue"
+import ModalDruzinaAdd from "@/components/admin/druzina/ModalDruzinaAdd.vue"
+
+import { useDruzineStore } from "@/stores/admin/useDruzineStore"
+import { useModal } from "@/composables/useModal"
+import { storeToRefs } from "pinia"
+
+const druzineStore = useDruzineStore()
+const { fetchMore, refreshPagination } = druzineStore
+const { druzine } = storeToRefs(druzineStore)
+const { openModal } = useModal()
+
+onBeforeMount(async () => {
+  await fetchMore()
 })
 </script>
