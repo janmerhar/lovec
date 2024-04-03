71a98980efa6c9512eeb34d97c0225145c1969e2
Jan Merhar <merharjan@gmail.com>
2024-04-03 21:09:38 +0200

    Add CardDruzina.vue

---
+++ src/client/src/components/admin/druzina/CardDruzina.vue
@@ -0,0 +1,37 @@
+<template>
+  <card-normal>
+    <text-card-title>
+      {{ druzina.ime }}
+    </text-card-title>
+    <text-card-subtitle>
+      <span style="margin-right: 0.5rem">
+        <font-awesome-icon
+          :icon="['fas', 'house-user']"
+          size="xl"
+          fixed-width
+        />
+        {{ druzina.claniCount }}
+      </span>
+      ·
+      <span style="margin-left: 0.5rem">
+        <font-awesome-icon :icon="['fas', 'map']" size="xl" fixed-width />
+        {{ druzina.revirjiCount }}
+      </span>
+    </text-card-subtitle>
+  </card-normal>
+</template>
+
+<script setup lang="ts">
+import CardNormal from "@/components/ui-components/card/CardNormal.vue"
+import TextCardTitle from "@/components/ui-components/card/TextCardTitle.vue"
+import TextCardSubtitle from "@/components/ui-components/card/TextCardSubtitle.vue"
+
+import type { DruzinaDetails } from "@/types"
+
+defineProps({
+  druzina: {
+    type: Object as () => DruzinaDetails,
+    required: true,
+  },
+})
+</script>
