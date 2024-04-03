2866e8936d41eac6aa0ecb8c701f62c8cde71036
Jan Merhar <merharjan@gmail.com>
2024-04-03 23:31:25 +0200

    Update TabAdminDruzine.vue

--- src/client/src/views/admin/TabAdminDruzine.vue
+++ src/client/src/views/admin/TabAdminDruzine.vue
@@ -6,7 +6,16 @@
     <template #body>
       <TransitionGroup name="list" tag="ul">
         <li v-for="elDruzina in druzine" :key="elDruzina.id">
-          <card-druzina-details :druzina="elDruzina"></card-druzina-details>
+          <card-druzina
+            :druzina="elDruzina"
+            @izbrisi="
+              useAlert().confirmDangerAlert(
+                $t('admin_druzine.tab.crud.delete.header'),
+                $t('admin_druzine.tab.crud.delete.message'),
+                async () => await deleteItem(elDruzina)
+              )
+            "
+          ></card-druzina>
         </li>
       </TransitionGroup>
 
@@ -23,18 +32,19 @@
 import { onBeforeMount } from "vue"
 
 import FabButtonAdd from "@/components/FabButtonAdd.vue"
-import CardDruzinaDetails from "@/components/admin/druzina/CardDruzinaDetails.vue"
+import CardDruzina from "@/components/admin/druzina/CardDruzina.vue"
 import TabTemplate from "@/components/ui-components/tab/TabTemplate.vue"
 import TabHeader from "@/components/ui-components/tab/TabHeader.vue"
 import TabNoElements from "@/components/ui-components/tab/TabNoElements.vue"
 import ModalDruzinaAdd from "@/components/admin/druzina/ModalDruzinaAdd.vue"
 
+import { useAlert } from "@/composables/useAlert"
 import { useDruzineStore } from "@/stores/admin/useDruzineStore"
 import { useModal } from "@/composables/useModal"
 import { storeToRefs } from "pinia"
 
 const druzineStore = useDruzineStore()
-const { fetchMore, refreshPagination } = druzineStore
+const { fetchMore, refreshPagination, deleteItem } = druzineStore
 const { druzine } = storeToRefs(druzineStore)
 const { openModal } = useModal()
 
