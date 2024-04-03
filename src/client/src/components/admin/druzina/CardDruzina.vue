b79fe64c294fbf24e12b2a95bda5ca3dc516499e
Jan Merhar <merharjan@gmail.com>
2024-04-03 21:37:21 +0200

    Add CardDruzina.vue

--- src/client/src/components/admin/druzina/CardDruzina.vue
+++ src/client/src/components/admin/druzina/CardDruzina.vue
@@ -1,23 +1,36 @@
 <template>
   <card-normal>
-    <text-card-title>
-      {{ druzina.ime }}
-    </text-card-title>
-    <text-card-subtitle>
-      <span style="margin-right: 0.5rem">
-        <font-awesome-icon
-          :icon="['fas', 'house-user']"
-          size="xl"
-          fixed-width
-        />
-        {{ druzina.claniCount }}
-      </span>
-      ·
-      <span style="margin-left: 0.5rem">
-        <font-awesome-icon :icon="['fas', 'map']" size="xl" fixed-width />
-        {{ druzina.revirjiCount }}
-      </span>
-    </text-card-subtitle>
+    <template #right-top>
+      <button-round color="danger" @click="emit('izbrisi')">
+        <font-awesome-icon :icon="['fas', 'trash']" size="xl" fixed-width />
+      </button-round>
+    </template>
+    <template #default>
+      <text-card-title>
+        {{ druzina.ime }}
+      </text-card-title>
+      <text-card-subtitle>
+        <span style="margin-right: 0.5rem">
+          <font-awesome-icon
+            :icon="['fas', 'house-user']"
+            size="xl"
+            fixed-width
+            style="margin-right: 0.25rem"
+          />
+          {{ druzina.claniCount }}
+        </span>
+        ·
+        <span style="margin-left: 0.5rem">
+          <font-awesome-icon
+            :icon="['fas', 'map']"
+            size="xl"
+            fixed-width
+            style="margin-right: 0.25rem"
+          />
+          {{ druzina.revirjiCount }}
+        </span>
+      </text-card-subtitle>
+    </template>
   </card-normal>
 </template>
 
@@ -25,6 +38,7 @@
 import CardNormal from "@/components/ui-components/card/CardNormal.vue"
 import TextCardTitle from "@/components/ui-components/card/TextCardTitle.vue"
 import TextCardSubtitle from "@/components/ui-components/card/TextCardSubtitle.vue"
+import ButtonRound from "@/components/ui-components/button/ButtonRound.vue"
 
 import type { DruzinaDetails } from "@/types"
 
@@ -34,4 +48,6 @@ defineProps({
     required: true,
   },
 })
+
+const emit = defineEmits(["izbrisi"])
 </script>
