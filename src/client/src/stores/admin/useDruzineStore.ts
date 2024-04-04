7eca19c693ac48d56295a40df7f1355c51ba0394
Jan Merhar <merharjan@gmail.com>
2024-04-04 22:41:45 +0200

    Add select in useDruzinaStore.ts

--- src/client/src/stores/admin/useDruzineStore.ts
+++ src/client/src/stores/admin/useDruzineStore.ts
@@ -6,6 +6,7 @@ import { useRequest } from "@/composables/useRequest"
 import { usePagination } from "@/composables/usePagination"
 import { useCRUD } from "@/composables/useCRUD"
 import { useAlert } from "@/composables/useAlert"
+import { useSelect } from "@/composables/useSelect"
 
 import i18n from "@/locales/i18n"
 const { t } = i18n.global
@@ -19,6 +20,7 @@ export const useDruzineStore = defineStore("druzine", () => {
     return response.data
   }
 
+  // Pagination
   const { items, fetchMore, refreshPagination } = usePagination<DruzinaDetails>(
     getDruzine,
     false
@@ -29,9 +31,51 @@ export const useDruzineStore = defineStore("druzine", () => {
     () => druzineVariable.value
   )
 
+  // CRUD
+  const deleteDruzina = async (druzina: DruzinaDetails): Promise<boolean> => {
+    const response = await request.delete<boolean>(`/druzine/${druzina.id}`)
+
+    return response.data
+  }
+
+  const crud = useCRUD<DruzinaDetails>(druzineVariable)
+  const { createItem, deleteItem, updateItem } = crud
+
+  const toastDelete = async (druzina: DruzinaDetails) => {
+    const call = async () => await deleteItem(deleteDruzina)(druzina)
+
+    const { successToastAction } = useAlert()
+
+    successToastAction(
+      call,
+      t("admin_druzine.tab.crud.delete.success", { name: druzina.ime })
+    )
+  }
+
+  // Select
+  const fetchDruzina = async (
+    druzina: DruzinaDetails | string
+  ): Promise<Druzina | null> => {
+    const id = typeof druzina === "string" ? druzina : druzina.id
+
+    const response = await request.get<Druzina>(`/druzine/${id}`)
+
+    return response.data
+  }
+
+  const { selectedItem, selectItem, fetchItem } =
+    useSelect<DruzinaDetails | null>(null)
+
   return {
     druzine,
     fetchMore,
     refreshPagination,
+    // CRUD
+    deleteItem: toastDelete,
+    // Select
+    selectedItem,
+    selectItem,
+    // @ts-ignore
+    fetchItem: fetchItem(fetchDruzina),
   }
 })
