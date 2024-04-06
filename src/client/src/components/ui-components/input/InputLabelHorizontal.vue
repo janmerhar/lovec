18afec021b9c30f4fa85db4ce9bfbd582d956e4d
Jan Merhar <merharjan@gmail.com>
2024-04-06 17:38:04 +0200

    Add InputLabelHorizontal.vue

---
+++ src/client/src/components/ui-components/input/InputLabelHorizontal.vue
@@ -0,0 +1,72 @@
+<template>
+  <div style="margin-top: 1rem; margin-bottom: 1rem">
+    <!-- INPUT and LEFT and/or RIGHT area -->
+    <div id="ilh-row">
+      <!-- LEFT side -->
+      <div id="ilh-left">
+        <slot name="left"></slot>
+      </div>
+      <div id="ilh-input" class="all">
+        <slot name="input"></slot>
+      </div>
+      <!-- RIGHT side -->
+      <div id="ilh-right">
+        <slot name="right"></slot>
+      </div>
+    </div>
+
+    <!-- ERROR at the bottom of input -->
+    <div id="ilh-error" class="all">
+      <slot name="error"></slot>
+    </div>
+  </div>
+</template>
+
+<style>
+#ilh-row {
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  background-color: var(--ion-color-step-250);
+  margin-left: 10px;
+  margin-right: 10px;
+  border-radius: 1000px;
+  height: 2.5rem;
+}
+
+#ilh-left {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 20%;
+  margin-left: 12px;
+}
+
+#ilh-input {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 60%;
+  width: 100%;
+  height: 100%;
+}
+
+#ilh-input > input {
+  background-color: var(--ion-color-step-250);
+  width: 100%;
+  height: 100%;
+  border: 0;
+  padding: 4px 4px;
+  margin-top: 2px;
+  margin-bottom: 2px;
+  font-size: 1rem;
+}
+
+#ilh-right {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 20%;
+  margin-right: 12px;
+}
+</style>
