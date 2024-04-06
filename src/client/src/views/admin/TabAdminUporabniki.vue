04f48599920767dbe63c9dee2cc00daa2c9af215
Jan Merhar <merharjan@gmail.com>
2024-04-06 18:01:01 +0200

    Update TabAdminUporabniki.vue

--- src/client/src/views/admin/TabAdminUporabniki.vue
+++ src/client/src/views/admin/TabAdminUporabniki.vue
@@ -4,6 +4,29 @@
       <tab-header>{{ $t("admin_uporabniki.tab.header") }}</tab-header>
     </template>
     <template #body>
+      <input-label-horizontal>
+        <template #input>
+          <Field
+            name="search"
+            type="text"
+            v-model="search"
+            @keyup.enter="searchItem(search)"
+            :placeholder="$t('admin_uporabniki.tab.search')"
+          />
+        </template>
+        <template #right>
+          <font-awesome-icon
+            :icon="['fas', 'magnifying-glass']"
+            fixed-width
+            @click="searchItem(search)"
+            style="cursor: pointer"
+          />
+        </template>
+        <template #error>
+          <ErrorMessage name="search" />
+        </template>
+      </input-label-horizontal>
+
       <TransitionGroup name="list" tag="ul">
         <li v-for="uporabnik in uporabniki" :key="uporabnik.id">
           <card-uporabnik
@@ -28,13 +51,15 @@
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount } from "vue"
+import { onBeforeMount, ref } from "vue"
 
 import FabButtonAdd from "@/components/FabButtonAdd.vue"
 import TabTemplate from "@/components/ui-components/tab/TabTemplate.vue"
 import TabHeader from "@/components/ui-components/tab/TabHeader.vue"
 import TabNoElements from "@/components/ui-components/tab/TabNoElements.vue"
 import CardUporabnik from "@/components/admin/uporabnik/CardUporabnik.vue"
+import InputLabelHorizontal from "@/components/ui-components/input/InputLabelHorizontal.vue"
+import { Form, Field, ErrorMessage } from "vee-validate"
 
 import { useUporabnikiStore } from "@/stores/admin/useUporabnikiStore"
 import { storeToRefs } from "pinia"
@@ -42,9 +67,11 @@ import { useAlert } from "@/composables/useAlert"
 import { useTabNavigation } from "@/composables/useTabNavigation"
 const uporabnikiStore = useUporabnikiStore()
 
-const { fetchMore, refreshPagination, deleteItem } = uporabnikiStore
+const { fetchMore, refreshPagination, deleteItem, searchItem } = uporabnikiStore
 const { uporabniki } = storeToRefs(uporabnikiStore)
 const { redirectTo } = useTabNavigation()
+
+const search = ref<string>("")
 
 onBeforeMount(async () => {
   await fetchMore()
