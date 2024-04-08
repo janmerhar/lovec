113157bec02e6c5c87704b9f35286b4d9da83d99
Jan Merhar <merharjan@gmail.com>
2024-04-08 22:30:04 +0200

    Add ListContainer.vue

---
+++ src/client/src/components/ui-components/list/ListContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="title">
+    <slot name="title"></slot>
+  </div>
+
+  <div class="list">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.list {
+  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+}
+
+.title {
+  margin: 1rem;
+  font-size: 1rem;
+  font-weight: bold;
+}
+</style>
