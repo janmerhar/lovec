b2b1984c23a439511e00f63653708f1e29b75714
Jan Merhar <merharjan@gmail.com>
2024-04-09 22:21:55 +0200

    Add TabAdminUporabnikVpleni.vue

--- src/client/src/views/admin/TabAdminUporabnikVpleni.vue
+++ src/client/src/views/admin/TabAdminUporabnikVpleni.vue
@@ -1,9 +1,48 @@
-<template>tab admin uporabnik vpleni</template>
+<template>
+  <tab-template :refresh="refreshPagination" :scroll="fetchMore">
+    <template #header>
+      <tab-header>
+        {{ $t("section.tab.header") }} {{ uporabnik?.ime }}
+        {{ uporabnik?.priimek }}
+      </tab-header>
+    </template>
+    <template #body>
+      <TransitionGroup
+        name="list"
+        tag="ul"
+        v-for="vplen in items"
+        :key="vplen.datum"
+      >
+        <li>
+          <card-vplen-details :vplen="vplen"> </card-vplen-details>
+        </li>
+      </TransitionGroup>
+
+      <tab-no-elements :elements="items"></tab-no-elements>
+    </template>
+  </tab-template>
+</template>
 
 <script setup lang="ts">
-import { onMounted } from "vue"
+import { onBeforeMount } from "vue"
+import { useUporabnikiStore } from "@/stores/admin/useUporabnikiStore"
+import { storeToRefs } from "pinia"
+import { useUporabnikVpleniStore } from "@/stores/admin/useUporabnikVpleniStore"
 
-onMounted(() => {
-  console.log("TabAdminDruzine.vue mounted")
+import TabTemplate from "@/components/ui-components/tab/TabTemplate.vue"
+import TabHeader from "@/components/ui-components/tab/TabHeader.vue"
+import TabNoElements from "@/components/ui-components/tab/TabNoElements.vue"
+import CardVplenDetails from "@/components/vplen/CardVplenDetails.vue"
+
+const uporabnikStore = useUporabnikiStore()
+const { selectedItem: uporabnik } = storeToRefs(uporabnikStore)
+
+const uporabniVpleniStore = useUporabnikVpleniStore()
+const { fetchMore, refreshPagination } = uporabniVpleniStore
+const { items } = storeToRefs(uporabniVpleniStore)
+
+onBeforeMount(async () => {
+  console.log("banana")
+  await fetchMore()
 })
 </script>
