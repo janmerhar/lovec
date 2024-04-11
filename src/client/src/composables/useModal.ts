4560de641d4da7d3d5553893af132a27de3ff68d
Jan Merhar <merharjan@gmail.com>
2024-04-11 22:44:36 +0200

    Update useModal.ts

--- src/client/src/composables/useModal.ts
+++ src/client/src/composables/useModal.ts
@@ -19,10 +19,27 @@ export const useModal = () => {
   const confirmModal = async <T = null>(data: T | null = null) => {
     return modalController.dismiss(data, "confirm")
   }
+  const openSheetModal = async (
+    component: any,
+    componentProps: object = {}
+  ) => {
+    const modal = await modalController.create({
+      component: component,
+      componentProps: componentProps,
+      breakpoints: [0, 0.5, 0.75, 1],
+      initialBreakpoint: 0.5,
+      handle: true,
+    })
+
+    await modal.present()
+
+    return modal.onWillDismiss()
+  }
 
   return {
     openModal,
     cancelModal,
     confirmModal,
+    openSheetModal,
   }
 }
