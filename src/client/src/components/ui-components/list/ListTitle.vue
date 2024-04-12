636e32f1a1aacdc16397a2645d1919b616db7010
Jan Merhar <merharjan@gmail.com>
2024-04-12 21:28:03 +0200

    Add ListTitle.vue

---
+++ src/client/src/components/ui-components/list/ListTitle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main">
+    <div class="start">
+      <slot></slot>
+    </div>
+
+    <div class="end">
+      <slot name="end"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0.25rem;
+  padding-right: 0.25rem;
+  color: var(--ion-text-color);
+}
+
+.start {
+  text-align: left;
+}
+
+.middle {
+  text-align: center;
+}
+
+.end {
+  text-align: right;
+}
+</style>
