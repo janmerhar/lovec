5d7fe5bab793c18dba4e8cd105c85b8b69ad21f5
Jan Merhar <merharjan@gmail.com>
2024-04-13 13:34:33 +0200

    Add ButtonOval.vue

---
+++ src/client/src/components/ui-components/button/ButtonOval.vue
@@ -0,0 +1,22 @@
+<template>
+  <ion-button :color="color">
+    <slot></slot>
+  </ion-button>
+</template>
+
+<script setup lang="ts">
+import { IonButton } from "@ionic/vue"
+
+defineProps({
+  color: {
+    type: String,
+    default: "success",
+  },
+})
+</script>
+
+<style scoped>
+ion-button {
+  --border-radius: 100000rem;
+}
+</style>
