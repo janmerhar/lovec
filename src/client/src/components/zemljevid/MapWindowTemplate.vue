ad3c33ad0482614a9efbf0f418c9ac87ab0b6583
Jan Merhar <merharjan@gmail.com>
2024-04-14 14:25:17 +0200

    Add MapWindowTemplate.vue

---
+++ src/client/src/components/zemljevid/MapWindowTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="position: relative">
+    <slot></slot>
+    <div
+      style="
+        top: 50%;
+        left: 50%;
+        border-radius: 100000000000rem;
+        position: absolute;
+        z-index: 69;
+        transform: translate(-50%, -50%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        pointer-events: none;
+      "
+    >
+      <font-awesome-icon
+        style="color: var(--ion-color-tertiary)"
+        :icon="['fas', 'plus']"
+      />
+    </div>
+  </div>
+</template>
