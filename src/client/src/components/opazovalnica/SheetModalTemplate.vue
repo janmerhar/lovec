f8e2a6f7804ec5af2ce234276fa83e279c890c3e
Jan Merhar <merharjan@gmail.com>
2024-04-16 23:01:52 +0200

    Add SheetModalTemplate.vue

---
+++ src/client/src/components/opazovalnica/SheetModalTemplate.vue
@@ -0,0 +1,35 @@
+<template>
+  <sheet-modal-template>
+    <template #header>
+      <sheet-modal-header>
+        {{ $t("oprema.crud.create.header") }}
+      </sheet-modal-header>
+    </template>
+
+    <!--  -->
+    <template #body> </template>
+    <infinite-scroll-component
+      :scroll="scroll"
+      v-if="true"
+    ></infinite-scroll-component>
+    <!--  -->
+  </sheet-modal-template>
+</template>
+
+<script setup lang="ts">
+import SheetModalTemplate from "@/components/ui-components/modal/SheetModalTemplate.vue"
+import SheetModalHeader from "@/components/ui-components/modal/SheetModalHeader.vue"
+
+defineProps({
+  scroll: {
+    type: Function,
+    default: () => {
+      return null
+    },
+  },
+  isRefreshable: {
+    type: Boolean,
+    default: true,
+  },
+})
+</script>
