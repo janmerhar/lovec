6ff90ef4ffcf08aa2196c91d961459186775577b
Jan Merhar <merharjan@gmail.com>
2024-04-17 21:25:38 +0200

    Add ImageProfile.vue

---
+++ src/client/src/components/ui-components/image/ImageProfile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="center">
+    <img
+      :src="slika"
+      alt=""
+      style="width: 25vh; height: 25vh; object-fit: cover; border-radius: 50%"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  slika: String,
+})
+</script>
+
+<style scoped>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+}
+</style>
