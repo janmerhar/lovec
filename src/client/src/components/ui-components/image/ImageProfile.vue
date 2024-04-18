f0ba73a102631a9de547b23ca78cedc5556c6506
Jan Merhar <merharjan@gmail.com>
2024-04-18 21:58:29 +0200

    Update ImageProfile.vue

--- src/client/src/components/ui-components/image/ImageProfile.vue
+++ src/client/src/components/ui-components/image/ImageProfile.vue
@@ -1,10 +1,15 @@
 <template>
+  <!-- TODO: naredi stil za image update -->
+  <!-- Torej neka obroba in zraven ikonca za spremembo -->
   <div class="center">
     <img
       :src="slika"
       alt=""
       style="width: 25vh; height: 25vh; object-fit: cover; border-radius: 50%"
     />
+  </div>
+  <div style="text-align: center">
+    <slot></slot>
   </div>
 </template>
 
@@ -19,7 +24,7 @@ defineProps({
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 2rem;
+  margin-top: 1.5rem;
   margin-bottom: 2rem;
 }
 </style>
