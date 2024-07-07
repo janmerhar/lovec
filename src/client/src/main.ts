8f538488f499a8cf1751dd77d5089cc8d954399d
Jan Merhar <merharjan@gmail.com>
2024-07-07 17:37:11 +0200

    Upgrade Ionic 7 -> 8

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -39,6 +39,17 @@ import "@ionic/vue/css/text-transformation.css"
 import "@ionic/vue/css/flex-utils.css"
 import "@ionic/vue/css/display.css"
 
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* @import '@ionic/vue/css/palettes/dark.always.css'; */
+/* @import '@ionic/vue/css/palettes/dark.class.css'; */
+import "@/theme/dark.class.css"
+
 /* Theme variables */
 import "./theme/variables.css"
 
