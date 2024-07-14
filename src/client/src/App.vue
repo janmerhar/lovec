93b29de0baea92d1358d25ca924f0446cdd3edf4
Jan Merhar <merharjan@gmail.com>
2024-07-14 14:27:49 +0200

    Update App.vue

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -7,4 +7,14 @@
 <script setup lang="ts">
 import "leaflet/dist/leaflet.css"
 import { IonApp, IonRouterOutlet } from "@ionic/vue"
+
+import { storeToRefs } from "pinia"
+import { usePreferencesStore } from "@/stores/usePreferencesStore"
+import type { Locale } from "@/locales/i18n"
+
+const preferencesStore = usePreferencesStore()
+const { locale } = storeToRefs(preferencesStore)
+const { selectLanguage } = preferencesStore
+
+selectLanguage(locale.value as Locale)
 </script>
