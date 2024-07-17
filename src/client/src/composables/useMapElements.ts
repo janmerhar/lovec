22e0c175b61c1ee2335de741ce55002a45f933bf
Jan Merhar <merharjan@gmail.com>
2024-07-17 22:03:38 +0200

    Update icon for vplen

--- src/client/src/composables/useMapElements.ts
+++ src/client/src/composables/useMapElements.ts
@@ -20,15 +20,11 @@ export const useMapElements = () => {
   })
 
   const iconVplen = (zival: string) => {
+    const vplen = require("../../public/assets/map/vplen/vplen.svg")
+
     return new Icon({
-      iconUrl:
-        "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
-      shadowUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41],
+      iconUrl: vplen,
+      iconSize,
     })
   }
 
