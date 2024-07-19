1452ca299ed315c4fbfd644d1d3e4d853fa447e4
Jan Merhar <merharjan@gmail.com>
2024-07-19 21:34:51 +0200

    Update useMapElements.ts

--- src/client/src/composables/useMapElements.ts
+++ src/client/src/composables/useMapElements.ts
@@ -1,23 +1,61 @@
+import type { Opazovalnica } from "@/types"
 import { Icon } from "leaflet"
+import type { PointExpression } from "leaflet"
 
-// Ikone za opazovalnice
-// - zasedena -> ti mogoce ne gres skozi, ker ne dobivam podtkov o zasedenosti
-// - trenutno rezervirana
-// - omogoca prespanje
-// - izbrisana ...
-// Mogoce se kaj za same revirje
+import { storeToRefs } from "pinia"
+
+import { useActiveObiskStore } from "@/stores/useActiveObiskStore"
+import { useLoginStore } from "@/stores/useLoginStore"
 
 export const useMapElements = () => {
-  const iconOpazovalnica = new Icon({
-    iconUrl:
-      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
-    shadowUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-  })
+  const iconSize: PointExpression = [40, 55]
+
+  const { selectedItem: activeObisk } = storeToRefs(useActiveObiskStore())
+  const { isAdmin } = storeToRefs(useLoginStore())
+
+  const opazovalnicaIzbrisana = require("../../public/assets/map/opazovalnica/opazovalnica-izbrisana.svg")
+  const opazovalnicaPolna = require("../../public/assets/map/opazovalnica/opazovalnica-polna.svg")
+  const opazovalnicaPrespanje = require("../../public/assets/map/opazovalnica/opazovalnica-prespanje.svg")
+
+  const opazovalnicaRezervirana = require("../../public/assets/map/opazovalnica/opazovalnica-rezervirana.svg")
+  const opazovalnicaOg = require("../../public/assets/map/opazovalnica/opazovalnica.svg")
+
+  const iconOpazovalnica = (opazovalnica: Opazovalnica) => {
+    // User
+    // 1. Opazovalnica
+    let icon = opazovalnicaOg
+
+    // 2. Prespanje
+    if (opazovalnica.prespanje) {
+      icon = opazovalnicaPrespanje
+    }
+
+    // 3. Polna
+    if (opazovalnica.kapaciteta === opazovalnica?.zasedenost) {
+      icon = opazovalnicaPolna
+    }
+
+    // 4. Zasedena
+    if (
+      !isAdmin.value &&
+      opazovalnica.id === activeObisk.value?.opazovalnica.id
+    ) {
+      icon = opazovalnicaRezervirana
+    }
+
+    // Admin
+    // 1. Izbrisana
+    if (isAdmin.value) {
+      if (opazovalnica.isDeleted) {
+        icon = opazovalnicaIzbrisana
+      }
+    }
+
+    return new Icon({
+      iconUrl: icon,
+      iconSize,
+    })
+  }
 
   const iconVplen = (zival: string) => {
     const vplen = require("../../public/assets/map/vplen/vplen.svg")
